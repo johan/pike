0ace837e92b306212af77d03cdbde7832d5b9b01
Henrik Grubbström (Grubba) <grubba@grubba.org>
1996-11-05 15:10:09 +0000

    Initial revision

---
+++ src/modules/_Crypto/crypto.h
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ *
+ * The Pike crypto module
+ *
+ * Henrik Grubbström 1996-10-24
+ *
+ * This module provides the Pike interface to libcrypto.so.
+ *
+ * NOTE:
+ *	If the crypto module was compiled as a dynamic module (the default),
+ *	you MUST include this file to get it loaded.
+ */
+
