ac49177d6d82dd6f4bb2325643e38f5244a17a2c
Henrik Grubbström (Grubba) <grubba@grubba.org>
1996-11-13 15:26:51 +0000

    Decryption is now done in the reverse order of encryption.

--- src/modules/_Crypto/pipe.c
+++ src/modules/_Crypto/pipe.c
@@ -185,6 +185,7 @@ static void f_set_encrypt_key(INT32 args)
   if (args != PIKE_PIPE->num_objs) {
     error("Wrong number of arguments to pipe->set_encrypt_key()\n");
   }
+  PIKE_PIPE->mode = 0;
   for (i=-args; i; i++) {
     int n_args;
 
@@ -211,6 +212,7 @@ static void f_set_decrypt_key(INT32 args)
   if (args != PIKE_PIPE->num_objs) {
     error("Wrong number of arguments to pipe->set_decrypt_key()\n");
   }
+  PIKE_PIPE->mode = 1;
   for (i=-args; i; i++) {
     int n_args;
 
@@ -243,8 +245,15 @@ static void f_crypt_block(INT32 args)
   if (sp[-1].u.string->len % PIKE_PIPE->block_size) {
     error("Bad length of argument 1 to pipe->crypt_block()\n");
   }
-  for (i=0; i<PIKE_PIPE->num_objs; i++) {
-    safe_apply(PIKE_PIPE->objects[i], "crypt_block", 1);
+  if (PIKE_PIPE->mode) {
+    /* Decryption -- Reverse the order */
+    for (i=PIKE_PIPE->num_objs; i--;) {
+      safe_apply(PIKE_PIPE->objects[i], "crypt_block", 1);
+    }
+  } else {
+    for (i=0; i<PIKE_PIPE->num_objs; i++) {
+      safe_apply(PIKE_PIPE->objects[i], "crypt_block", 1);
+    }
   }
 }
 
