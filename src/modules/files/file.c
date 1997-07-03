2ea5e13ee7aa77fc7af8b8fab3056eba7c11c420
Henrik Grubbström (Grubba) <grubba@grubba.org>
1997-07-03 02:36:16 +0000

    Fixed return-type of file->dup2();

--- src/modules/files/file.c
+++ src/modules/files/file.c
@@ -1521,7 +1521,7 @@ void pike_module_init()
   add_function("query_close_callback",file_query_close_callback,"function(:mixed)",0);
 
   add_function("dup",file_dup,"function(:object)",0);
-  add_function("dup2",file_dup2,"function(object:object)",0);
+  add_function("dup2",file_dup2,"function(object:int)",0);
   add_function("assign",file_assign,"function(object:int)",0);
   add_function("pipe",file_pipe,"function(:object)",0);
 
