318304790765236d5ea0702041e0787d4b607c8a
Henrik Grubbström (Grubba) <grubba@grubba.org>
1997-10-14 01:02:38 +0000

    Removed warning.

--- src/modules/spider/dumudp.c
+++ src/modules/spider/dumudp.c
@@ -133,12 +133,13 @@ static void udp_bind(INT32 args)
 
 void udp_enable_broadcast(INT32 args)
 {
-  int o;
-  pop_n_elms(args);
 #ifdef SO_BROADCAST
+  int o;
+  pop_n_elems(args);
   o = 1;
   push_int(setsockopt(fd, SOL_SOCKET, SO_BROADCAST, (char *)&o, sizeof(int)));
 #else /* SO_BROADCAST */
+  pop_n_elems(args);
   push_int(0);
 #endif /* SO_BROADCAST */
 }
