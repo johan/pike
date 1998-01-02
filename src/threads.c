65d0ee9d33caef98e08b30f1338f46ca53582eee
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
1998-01-02 08:18:43 +0000

    bugfixes

--- src/threads.c
+++ src/threads.c
@@ -532,7 +532,7 @@ void th_init(void)
   set_gc_mark_callback(thread_was_marked);
 #endif
   set_init_callback(init_thread_obj);
-  set_init_callback(exit_thread_obj);
+  set_exit_callback(exit_thread_obj);
   thread_id_prog=end_program();
   if(!mutex_key)
     fatal("Failed to initialize thread program!\n");
