c0b1e4256fe85c86556431899c4f1d5ac9ae9a8e
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
1998-02-19 21:38:45 +0000

    bugfix in gc

--- src/object.c
+++ src/object.c
@@ -922,9 +922,6 @@ void gc_mark_object_as_referenced(struct object *o)
       if(frame.context.prog->gc_marked)
 	frame.context.prog->gc_marked(o);
 
-      if(frame.context.parent)
-	gc_mark_object_as_referenced(frame.context.parent);
-
       for(d=0;d<(int)frame.context.prog->num_identifiers;d++)
       {
 	if(!IDENTIFIER_IS_VARIABLE(frame.context.prog->identifiers[d].identifier_flags)) 
@@ -959,7 +956,7 @@ void gc_check_all_objects(void)
   {
 #ifdef DEBUG
     if(o->parent)
-      if(gc_check(o->parent)==-2)
+      if(debug_gc_check(o->parent,T_OBJECT,o)==-2)
 	fprintf(stderr,"(in object at %lx -> parent)\n",(long)o);
 #else
     if(o->parent)
@@ -973,9 +970,6 @@ void gc_check_all_objects(void)
       {
 	struct inherit in=o->prog->inherits[e];
 	char *base=o->storage + in.storage_offset;
-
-	if(in.parent)
-	  gc_check(in.parent);
 
 	for(d=0;d<in.prog->num_identifiers;d++)
 	{
