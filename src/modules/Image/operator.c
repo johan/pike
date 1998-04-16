5630f5a3d46582222835a9df1012b4985a579694
Mirar (Pontus Hagland) <pike@sort.mirar.org>
1998-04-16 04:32:51 +0000

    bugfix in ==

--- src/modules/Image/operator.c
+++ src/modules/Image/operator.c
@@ -410,6 +410,7 @@ void image_operator_equal(INT32 args)
    if (s1==s2)
    {
       pop_n_elems(args);
+fprintf(stderr,"ok 1\n");
       push_int(1);
       return; /* same image is equal */
    }
@@ -419,7 +420,7 @@ void image_operator_equal(INT32 args)
    if (s2)
       while (i--)
       {
-	 if (s1->r!=s1->r || s1->g!=s1->g || s1->b!=s1->b) { res=0; break; }
+	 if (s1->r!=s2->r || s1->g!=s2->g || s1->b!=s2->b) { res=0; break; }
 	 s1++; s2++;
       }
    else
