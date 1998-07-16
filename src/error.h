4c6bc6ce26dfe5a62d74c64166bafabe53d02544
Henrik Grubbström (Grubba) <grubba@grubba.org>
1998-07-16 21:56:15 +0000

    Added some LIVE debug.

--- src/error.h
+++ src/error.h
@@ -67,6 +67,8 @@ extern int throw_severity;
 #ifdef DEBUG
 #define SET_ONERROR(X,Y,Z) \
   do{ \
+     fprintf(stderr, "SET_ONERROR(%p, %p, %p) %s:%d\n", \
+             &(X), (Y), (Z), __FILE__, __LINE__); \
      if(!recoveries) break; \
      X.func=(error_call)(Y); \
      X.arg=(void *)(Z); \
@@ -77,6 +79,7 @@ extern int throw_severity;
   }while(0)
 
 #define UNSET_ONERROR(X) do {\
+    fprintf(stderr, "UNSET_ONERROR(%p) %s:%d\n", &(X), __FILE__, __LINE__); \
     if(!recoveries) break; \
     if(recoveries->onerror != &(X)) { \
       if (recoveries->onerror) { \
