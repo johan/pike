325a27ce5895c8d55d58c98d170409043dc51e2f
Henrik Grubbström (Grubba) <grubba@grubba.org>
1998-07-17 12:58:18 +0000

    We want to wait for system-calls in other threads in create_process().

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -1219,7 +1219,7 @@ void f_create_process(INT32 args)
     storage.argv=(char **)xalloc((1+cmd->size) * sizeof(char *));
 
 #if 1
-    init_threads_disable(NULL);
+    init_threads_disable(this_thread);
     storage.disabled = 1;
 #endif
 
