f87c1726bf901eff7d69bb9b7af7a0dccd673805
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
1999-01-29 12:29:02 +0000

    some optimization bugfixes

--- src/las.c
+++ src/las.c
@@ -1862,55 +1862,49 @@ static void optimize(node *n)
       break;
 
     case F_ADD_EQ:
-      if(CDR(n)->type == int_type_string)
+      if(CDR(n)->token == F_CONSTANT && CDR(n)->u.sval.type == T_INT)
       {
-	if(CDR(n)->token == F_CONSTANT && CDR(n)->u.sval.type == T_INT)
+	/* a+=0 -> a */
+	if(CDR(n)->u.sval.u.integer == 0) goto use_car;
+	
+	/* a+=1 -> ++a */
+	if(CDR(n)->u.sval.u.integer == 1)
 	{
-	  /* a+=0 -> a */
-	  if(n->u.sval.u.integer == 0) goto use_car;
-
-	  /* a+=1 -> ++a */
-	  if(n->u.sval.u.integer == 1)
-	  {
-	    tmp1=mknode(F_INC,CDR(n),0);
-	    CDR(n)=0;
-	    goto use_tmp1;
-	  }
-
-	  /* a+=-1 -> --a */
-	  if(n->u.sval.u.integer == -1)
-	  {
-	    tmp1=mknode(F_DEC, CDR(n), 0);
-	    CDR(n)=0;
-	    goto use_tmp1;
-	  }
+	  tmp1=mknode(F_INC,CAR(n),0);
+	  CAR(n)=0;
+	  goto use_tmp1;
+	}
+	
+	/* a+=-1 -> --a */
+	if(CDR(n)->u.sval.u.integer == -1)
+	{
+	  tmp1=mknode(F_DEC, CAR(n), 0);
+	  CAR(n)=0;
+	  goto use_tmp1;
 	}
       }
       break;
 
     case F_SUB_EQ:
-      if(CDR(n)->type == int_type_string)
+      if(CDR(n)->token == F_CONSTANT && CDR(n)->u.sval.type == T_INT)
       {
-	if(CDR(n)->token == F_CONSTANT && CDR(n)->u.sval.type == T_INT)
+	/* a-=0 -> a */
+	if(CDR(n)->u.sval.u.integer == 0) goto use_car;
+	
+	/* a-=-1 -> ++a */
+	if(CDR(n)->u.sval.u.integer == -1)
 	{
-	  /* a-=0 -> a */
-	  if(n->u.sval.u.integer == 0) goto use_car;
-
-	  /* a-=-1 -> ++a */
-	  if(n->u.sval.u.integer == -1)
-	  {
-	    tmp1=mknode(F_INC, CDR(n), 0);
-	    CDR(n)=0;
-	    goto use_tmp1;
-	  }
-
-	  /* a-=-1 -> --a */
-	  if(n->u.sval.u.integer == 1)
-	  {
-	    tmp1=mknode(F_DEC, CDR(n), 0);
-	    CDR(n)=0;
-	    goto use_tmp1;
-	  }
+	  tmp1=mknode(F_INC, CAR(n), 0);
+	  CAR(n)=0;
+	  goto use_tmp1;
+	}
+	
+	/* a-=-1 -> --a */
+	if(CDR(n)->u.sval.u.integer == 1)
+	{
+	  tmp1=mknode(F_DEC, CAR(n), 0);
+	  CAR(n)=0;
+	  goto use_tmp1;
 	}
       }
       break;
@@ -2066,14 +2060,15 @@ static void optimize(node *n)
 	if(!node_is_eq(*arg1,CAR(tmp1)) || /* x == z */
 	   depend_p(*arg2,*arg2) ||	/* does y depend on y? */
 	   depend_p(*arg2,*arg1) ||	/* does y depend on x? */
-	   depend_p(*arg2,CADR(n))) /* does y depend on p? */
+	   depend_p(*arg2,CADR(n)) ||   /* does y depend on p? */
+	   depend_p(*arg2,tmp1))        /* does y depend on z? */
 	{
-	  /* it was not on the form for(; x op y; x++) p; */
+	  /* it was not on the form for(; x op y; z++) p; */
 	  if(!node_is_eq(*arg2,CAR(tmp1)) || /* y == z */
-	     depend_p(*arg1,*arg2) || /* does x depend on y? */
-	     depend_p(*arg1,*arg1) || /* does x depend on x? */
-	     depend_p(*arg1,CADR(n)) /* does x depend on p? */
-	     )
+	     depend_p(*arg1,*arg2) ||   /* does x depend on y? */
+	     depend_p(*arg1,*arg1) ||   /* does x depend on x? */
+	     depend_p(*arg1,CADR(n)) || /* does x depend on p? */
+	     depend_p(*arg1,tmp1))      /* does x depend on z? */
 	  {
 	    /* it was not on the form for(; x op y; y++) p; */
 	    break;
