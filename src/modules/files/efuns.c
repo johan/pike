da104b7e8b24ca40247350a7d2e60742712107c6
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-02-15 20:53:09 +0000

    Workaround for glibc 2.1.

--- src/modules/files/efuns.c
+++ src/modules/files/efuns.c
@@ -3,6 +3,7 @@
 ||| Pike is distributed as GPL (General Public License)
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
+/**/
 #include "global.h"
 #include "fdlib.h"
 #include "interpret.h"
@@ -294,14 +295,16 @@ void f_filesystem_stat(INT32 args)
     push_int(st.f_ffree);
     push_text("favail");
     push_int(st.f_favail);
+#if 0
     push_text("fsname");
     push_text(st.f_fstr);
+#endif /* 0 */
 #ifdef HAVE_STATVFS_F_BASETYPE
     push_text("fstype");
     push_text(st.f_basetype);
-    f_aggregate_mapping(9*2);
-#else /* !HAVE_STATVFS_F_BASETYPE */
     f_aggregate_mapping(8*2);
+#else /* !HAVE_STATVFS_F_BASETYPE */
+    f_aggregate_mapping(7*2);
 #endif /* HAVE_STATVFS_F_BASETYPE */
 #else /* !HAVE_STATVFS */
 #ifdef HAVE_STATFS
