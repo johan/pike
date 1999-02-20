e1dd1e0406a986f12be47be2aa75cee7b1bddbf9
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-02-20 20:06:22 +0000

    Very minor optimization.

--- src/lexer.h
+++ src/lexer.h
@@ -710,8 +710,8 @@ static int low_yylex(YYSTYPE *yylval)
 	    if(ISWORD("object")) return F_OBJECT_ID;
 	  break;
 	  case TWO_CHAR('p','r'):
-	    if(ISWORD("predef")) return F_PREDEF;
 	    if(ISWORD("program")) return F_PROGRAM_ID;
+	    if(ISWORD("predef")) return F_PREDEF;
 	    if(ISWORD("private")) return F_PRIVATE;
 	    if(ISWORD("protected")) return F_PROTECTED;
 	    break;
@@ -726,8 +726,8 @@ static int low_yylex(YYSTYPE *yylval)
 	    if(ISWORD("sscanf")) return F_SSCANF;
 	  break;
 	  case TWO_CHAR('s','t'):
-	    if(ISWORD("static")) return F_STATIC;
 	    if(ISWORD("string")) return F_STRING_ID;
+	    if(ISWORD("static")) return F_STATIC;
 	  break;
 	  case TWO_CHAR('s','w'):
 	    if(ISWORD("switch")) return F_SWITCH;
