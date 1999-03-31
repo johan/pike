6898f2615d74f371098161fc4587104dca4d68f8
Mirar (Pontus Hagland) <pike@sort.mirar.org>
1999-03-31 18:36:53 +0000

    add, sub and multiply

--- src/modules/Math/math.c
+++ src/modules/Math/math.c
@@ -13,16 +13,23 @@
 
 /* add other parsers here */
 
+struct program *math_matrix_program;
+
 static struct math_class
 {
    char *name;
    void (*func)(void);
+   struct program **pd;
 } sub[] = {
-   {"Matrix",init_math_matrix},
+   {"Matrix",init_math_matrix,&math_matrix_program},
 };
 
 void pike_module_exit(void)
 {
+   int i;
+   for (i=0; i<(int)(sizeof(sub)/sizeof(sub[0])); i++)
+      if (sub[i].pd && sub[i].pd[0])
+	 free_program(sub[i].pd[0]);
 }
 
 void pike_module_init(void)
@@ -37,6 +44,7 @@ void pike_module_init(void)
       sub[i].func();
       p=end_program();
       add_program_constant(sub[i].name,p,0);
-      free_program(p);
+      if (sub[i].pd) sub[i].pd[0]=p;
+      else free_program(p);
    }
 }
