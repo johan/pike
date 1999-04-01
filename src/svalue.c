0d1dbba19d4d8a301e0fe0473151a78982557f76
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
1999-04-01 17:22:18 +0000

    easier-to-read debug

--- src/svalue.c
+++ src/svalue.c
@@ -810,6 +810,7 @@ void describe_svalue(struct svalue *s,int indent,struct processing *p)
 	      {
 		case '0': case '1': case '2': case '3':
 		case '4': case '5': case '6': case '7':
+		case '8': case '9':
 		  my_putchar('"');
 		  my_putchar('"');
 	      }
