11ea3370eb3018eaa1ff0bda3ab6e5dcb6554f0c
Mirar (Pontus Hagland) <pike@sort.mirar.org>
1999-06-18 19:19:29 +0000

    autoconverted a lot of 'bad arg' and 'out of memory' type errors

--- src/modules/Image/colors.c
+++ src/modules/Image/colors.c
@@ -678,7 +678,8 @@ static void image_color_cast(INT32 args)
 {
    if (args!=1 ||
        sp[-1].type!=T_STRING)
-      error("Image.Color.Color->cast(): Illegal argument(s)\n");
+      bad_arg_error("Image.Color.Color->cast",sp-args,args,0,"",sp-args,
+		"Bad arguments to Image.Color.Color->cast()\n");
    
    if (sp[-1].u.string==str_array)
    {
@@ -1238,7 +1239,8 @@ static void image_guess_color(INT32 args)
    struct svalue s;
 
    if (args!=1 && sp[-args].type!=T_STRING) 
-      error("Image.Color->guess(): illegal argument(s)\n");
+      bad_arg_error("Image.Color->guess",sp-args,args,0,"",sp-args,
+		"Bad arguments to Image.Color->guess()\n");
    
    f_lower_case(1);
    push_text(" ");
@@ -1460,7 +1462,8 @@ static void image_make_html_color(INT32 args)
    if (args!=1 ||
        sp[-1].type!=T_STRING) 
    {
-      error("Image.Color.html(): illegal arguments\n");
+      bad_arg_error("Image.Color.html",sp-args,args,0,"",sp-args,
+		"Bad arguments to Image.Color.html()\n");
       return;
    }
    
