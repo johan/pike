be33c31c3479457b41a99787a37db7ff7d16b0f8
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-09-06 12:03:57 +0000

    Added FIXME.

--- src/program.c
+++ src/program.c
@@ -1238,7 +1238,10 @@ void low_inherit(struct program *p,
     if(!parent && !parent_offset)
     {
       yyerror("Parent pointer lost, cannot inherit!");
-      /* We inherit it anyways, to avoid causing more errors */
+      /* We inherit it anyway, to avoid causing more errors */
+      /* FIXME: Should parent_offset be decreased below in this case too?
+       * /grubba 1999-09-06.
+       */
     }
   }
 
