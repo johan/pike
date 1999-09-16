4cc7d097721e42cd87f771d31e227bd157faa95f
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
1999-09-16 03:54:23 +0000

    more debug

--- src/program.c
+++ src/program.c
@@ -1078,6 +1078,7 @@ struct program *debug_end_program(void)
 SIZE_T low_add_storage(SIZE_T size, SIZE_T alignment, int modulo_orig)
 {
   long offset;
+  int modulo;
 #ifdef PIKE_DEBUG
   if(alignment <=0 || (alignment & (alignment-1)) || alignment > 256)
     fatal("Alignment must be 1,2,4,8,16,32,64,128 or 256 not %d\n",alignment);
