8f1ded9d0429d8b5eee7734fd94a8c270255179e
Fredrik Noring <noring@pike.ida.liu.se>
1999-10-15 21:06:19 +0000

    Small fix.  Reduction cannot be done before the integer is computed.

--- src/modules/Gmp/mpz_glue.c
+++ src/modules/Gmp/mpz_glue.c
@@ -478,7 +478,7 @@ static void mpzmod_sub(INT32 args)
 {
   INT32 e;
   struct object *res;
-  
+
   if (args)
     for (e = 0; e<args; e++)
       get_mpz(sp + e - args, 1);
@@ -770,8 +770,8 @@ static void mpzmod_sqrt(INT32 args)
     error("mpz->sqrt() on negative number.\n");
 
   o=clone_object(mpzmod_program,0);
-  PUSH_REDUCED(o);
   mpz_sqrt(OBTOMPZ(o), THIS);
+  PUSH_REDUCED(o);
 }
 
 static void mpzmod_sqrtrem(INT32 args)
