83e59e1f1717b199f9e22fbc734fae8e5749dd65
Fredrik Noring <noring@pike.ida.liu.se>
1999-10-15 23:53:44 +0000

    Added definition __AUTO_BIGNUM__.

--- src/cpp.c
+++ src/cpp.c
@@ -1138,6 +1138,9 @@ void f_cpp(INT32 args)
     simple_add_define(&this, "__MINOR__", buffer);
     sprintf(buffer, " %d ", PIKE_BUILD_VERSION);
     simple_add_define(&this, "__BUILD__", buffer);
+#ifdef AUTO_BIGNUM
+    simple_add_define(&this, "__AUTO_BIGNUM__", " 1 ");
+#endif
 #ifdef __NT__
     simple_add_define(&this, "__NT__", " 1 ");
 #endif
