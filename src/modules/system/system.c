4ee7fdf20186fe8d57bb8f6d3ab10758116b6664
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-10-27 15:16:40 +0000

    Fixed some typos.

--- src/modules/system/system.c
+++ src/modules/system/system.c
@@ -770,7 +770,7 @@ static struct {
   { "sysname", SI_SYSNAME },
 #endif /* SI_SYSNAME */
 #ifdef SI_HOSTNAME
-  { "hostname", SI_HOSTNAME },
+  { "nodename", SI_HOSTNAME },
 #endif /* SI_HOSTNAME */
 #ifdef SI_RELEASE
   { "release", SI_RELEASE },
@@ -879,12 +879,12 @@ void f_gethostname(INT32 args)
   gethostname(name, 1024);
   push_text(name);
 }
-#elif defined(HAVE_SYSINFO) && defined(SI_NODENAME)
+#elif defined(HAVE_SYSINFO) && defined(SI_HOSTNAME)
 void f_gethostname(INT32 args)
 {
   char name[1024];
   pop_n_elems(args);
-  if (sysinfo(SI_NODENAME, name, sizeof(name)) < 0) {
+  if (sysinfo(SI_HOSTNAME, name, sizeof(name)) < 0) {
     error("sysinfo() system call failed.\n");
   }
   push_text(name);
