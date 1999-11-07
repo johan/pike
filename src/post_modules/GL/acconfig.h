4061fb64ac2f00ac2ee4d06bccfd4a08cd933bc0
Per Hedbor <per@pike.ida.liu.se>
1999-11-07 01:33:42 +0000

    Module that require pike to compile

---
+++ src/post_modules/GL/acconfig.h
@@ -0,0 +1,16 @@
+/* $Id$ */
+
+/* Define if you have libGL */
+#undef HAVE_LIBGL
+
+/* Define if you have libOpenGL */
+#undef HAVE_LIBOPENGL
+
+/* Define if you have libOpenGL32 */
+#undef HAVE_LIBOPENGL32
+
+/* Define if you have libMesaGL */
+#undef HAVE_LIBMESAGL
+
+/* Define if you have GL */
+#undef HAVE_GL
