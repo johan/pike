cac671a209cd963e90f1347d072158cef7bb1d47
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-11-11 15:55:36 +0000

    Fixed warning.

--- src/las.c
+++ src/las.c
@@ -86,7 +86,9 @@ void check_tree(node *n, int depth)
   if(n->token==USHRT_MAX)
     fatal("Free node in tree.\n");
 
+#ifdef SHARED_NODES
   check_node_hash(n);
+#endif /* SHARED_NODES */
 
   if(d_flag<2) return;
 
