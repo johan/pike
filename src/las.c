e5de23fbfa74ec1f85c43f0304c25fa934c4abe3
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-11-12 19:03:32 +0000

    Disabled some debug.

--- src/las.c
+++ src/las.c
@@ -343,8 +343,12 @@ void free_all_nodes(void)
 		if(cdr_is_node(tmp)) _CDR(tmp)=0;
 #ifdef SHARED_NODES
 		tmp->hash = hash_node(tmp);
-
-		fprintf(stderr, "Freeing node that had %d refs.\n", tmp->refs);
+#ifdef PIKE_DEBUG
+		if (l_flag > 3) {
+		  fprintf(stderr, "Freeing node that had %d refs.\n",
+			  tmp->refs);
+		}
+#endif /* PIKE_DEBUG */
 		/* Force the node to be freed. */
 		tmp->refs = 1;
 #endif /* SHARED_NODES */
