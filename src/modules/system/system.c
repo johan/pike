b5e3416a3ec34b6f9d5878ac3685ef3ca9b7b760
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
1999-11-12 01:45:47 +0000

    bugfix for stupid hp-cc

--- src/modules/system/system.c
+++ src/modules/system/system.c
@@ -799,6 +799,7 @@ static struct {
 #ifdef SI_SRPC_DOMAIN
   { "srpc domain", SI_SRPC_DOMAIN },
 #endif /* SI_SRPC_DOMAIN */
+  { "TERMINATOR", 0 }
 };
 
 /* Recomended is >257 */
@@ -814,7 +815,7 @@ void f_uname(INT32 args)
 
   old_sp = sp;
 
-  for(i=0; i < NELEM(si_fields); i++) {
+  for(i=0; i < NELEM(si_fields)-1; i++) {
     long res;
     res = sysinfo(si_fields[i].command, buffer, PIKE_SI_BUFLEN);
 
