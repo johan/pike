fb25bd5bebe4ef5147721bb155f0565dd615e150
Per Hedbor <per@pike.ida.liu.se>
1999-11-14 00:47:21 +0000

    Initial version in pike

---
+++ src/modules/CommonLog/acconfig.h
@@ -0,0 +1,11 @@
+/*
+ * $Id$
+ */
+
+#ifndef COMMONLOG_MACHINE_H
+#define COMMONLOG_MACHINE_H
+
+@TOP@
+@BOTTOM@
+
+#endif /* COMMONLOG_MACHINE_H */
---
+++ src/modules/HTTPLoop/acconfig.h
@@ -0,0 +1,9 @@
+/*
+ * $Id$
+ */
+#undef CAN_HAVE_SENDFILE
+#undef CAN_HAVE_LINUX_SYSCALL4
+#undef CAN_HAVE_NONSHARED_LINUX_SYSCALL4
+
+/* Define if your sendfile() takes 7 args (FreeBSD style) */
+#undef HAVE_FREEBSD_SENDFILE
