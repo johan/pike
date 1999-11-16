c40edb965aad5706a8476362699c8b427472ee2e
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-11-16 16:42:10 +0000

    f_cast() takes the type as the first argument...

--- src/module_support.c
+++ src/module_support.c
@@ -169,8 +169,8 @@ int va_get_args(struct svalue *s,
         *va_arg(ap, int *)=(int)s->u.float_number;
       else 
       {
-        push_svalue( s );
         push_text( "int" );
+        push_svalue( s );
         f_cast( );
         *va_arg(ap, int *)=sp[-1].u.integer;
         pop_stack();
@@ -183,8 +183,8 @@ int va_get_args(struct svalue *s,
         *va_arg(ap, INT_TYPE *)=(int)s->u.float_number;
       else 
       {
-        push_svalue( s );
         push_text( "int" );
+        push_svalue( s );
         f_cast( );
         *va_arg(ap, INT_TYPE *)=sp[-1].u.integer;
         pop_stack();
@@ -227,8 +227,8 @@ int va_get_args(struct svalue *s,
 	 *va_arg(ap, float *)=(float)s->u.integer;
       else 
       {
-        push_svalue( s );
         push_text( "float" );
+        push_svalue( s );
         f_cast( );
         *va_arg(ap, float *)=sp[-1].u.float_number;
         pop_stack();
