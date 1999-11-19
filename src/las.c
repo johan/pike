efaee2ade79c52414ee5dd6e4d9ed78f0c4ec237
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-11-19 21:08:05 +0000

    Fixed memory leak.

--- src/las.c
+++ src/las.c
@@ -2025,7 +2025,9 @@ void fix_type_field(node *n)
     if (!CAR(n) || (CAR(n)->type == void_type_string)) {
       my_yyerror("Calling a void expression.");
       /* The optimizer converts this to an expression returning 0. */
-      copy_shared_string(n->type, mixed_type_string);
+      if (!n->type) {
+	copy_shared_string(n->type, mixed_type_string);
+      }
     } else {
     struct pike_string *s;
     struct pike_string *f;
