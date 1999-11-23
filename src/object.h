625b4c34793a691756c35ebea434e1d3358f3c74
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
1999-11-23 10:21:12 +0000

    Fixed inconsistent types in the master() macro with PIKE_DEBUG.

--- src/object.h
+++ src/object.h
@@ -115,7 +115,7 @@ void check_all_objects(void);
 #endif
 
 #ifdef PIKE_DEBUG
-#define master() ( get_master() ? get_master() : ( fatal("Couldn't load master object at %s:%d.\n",__LINE__,__FILE__), 0) )
+#define master() ( get_master() ? get_master() : ( fatal("Couldn't load master object at %s:%d.\n",__LINE__,__FILE__), NULL) )
 #else
 #define master() debug_master()
 #endif
