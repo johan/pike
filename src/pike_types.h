59b46a7fdd224ad9286ba776e7b33d6f1be74938
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
1999-12-14 19:51:57 +0000

    dtObjInher is really dtObjIs.

--- src/pike_types.h
+++ src/pike_types.h
@@ -183,7 +183,7 @@ int type_may_overload(char *type, int lfun);
 #define dtSet(IND) {unsafe_push_type(PIKE_T_MULTISET); {IND}}
 #define dtMultiset dtSet(dtMix)
 #define dtObjImpl(PROGRAM) {unsafe_push_type(PIKE_T_OBJECT); unsafe_push_type(0); push_type_int_backwards((PROGRAM)->id);}
-#define dtObjInher(PROGRAM) {unsafe_push_type(PIKE_T_OBJECT); unsafe_push_type(1); push_type_int_backwards((PROGRAM)->id);}
+#define dtObjIs(PROGRAM) {unsafe_push_type(PIKE_T_OBJECT); unsafe_push_type(1); push_type_int_backwards((PROGRAM)->id);}
 #define dtObj dtStore(tObj)
 #define dtFuncV(ARGS,REST,RET) MagicdtFuncV(RET,REST,ARGS)
 #define dtFunc(ARGS,RET) MagicdtFunc(RET,ARGS)
