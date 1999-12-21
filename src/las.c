1b6961795dfd8927c156f9a423d8243c0aeb960e
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-12-21 22:45:16 +0000

    Improved "Return type mismatch" warning.

--- src/las.c
+++ src/las.c
@@ -2441,7 +2441,14 @@ void fix_type_field(node *n)
 	}
 	else if (lex.pragmas & ID_STRICT_TYPES)
 	{
+	  struct pike_string *t1 =
+	    describe_type(compiler_frame->current_return_type);
+	  struct pike_string *t2 = describe_type(CAR(n)->type);
 	  yywarning("Return type mismatch.");
+	  yywarning("Expected: %s", t1->str);
+	  yywarning("Got     : %s", t2->str);
+	  free_string(t2);
+	  free_string(t1);
 	}
       }
     }
