3b9a4c29b448f3ab612b9492c2ad17ff234fd347
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2000-02-01 06:24:40 +0000

    better debug I hope..

--- src/gc.c
+++ src/gc.c
@@ -354,7 +354,7 @@ void describe_something(void *a, int t, int dm)
       fprintf(stderr,"**Describing mapping:\n");
       debug_dump_mapping((struct mapping *)a);
       fprintf(stderr,"**Describing mapping data block:\n");
-      describe_something( ((struct mapping *)a)->data, -1, dm);
+      describe_something( ((struct mapping *)a)->data, -2, dm);
       break;
 
     case T_STRING:
