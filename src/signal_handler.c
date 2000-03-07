5bbdc3fc8ef5f18605f3adf4759d131a35f4f953
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2000-03-07 21:23:13 +0000

    signal() now returns the old signal handler (if any)

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -788,9 +788,10 @@ static void f_signal(int args)
 #endif
     }
   }
+  assign_svalue(sp-args,signal_callbacks+signum);
   assign_svalue(signal_callbacks + signum, sp+1-args);
   my_signal(signum, func);
-  pop_n_elems(args);
+  pop_n_elems(args-1);
 }
 
 void set_default_signal_handler(int signum, void (*func)(INT32))
@@ -3310,7 +3311,7 @@ void init_signals(void)
            OPT_SIDE_EFFECT);
   
 /* function(int,mixed|void:void) */
-  ADD_EFUN("signal",f_signal,tFunc(tInt tOr(tMix,tVoid),tVoid),OPT_SIDE_EFFECT);
+  ADD_EFUN("signal",f_signal,tFunc(tInt tOr(tMix,tVoid),tMix),OPT_SIDE_EFFECT);
 #ifdef HAVE_KILL
   
 /* function(int|object,int:int) */
