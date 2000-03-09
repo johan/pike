ead15d4f64c567a9397b5c9d641a37d3dfbc60aa
Johan Sundström <jhs@pike.ida.liu.se>
2000-03-09 16:46:03 +0000

    __DATE__ should be "Mon DD YYYY", not " YYYYMon DD".

--- src/cpp.c
+++ src/cpp.c
@@ -846,8 +846,8 @@ static void insert_current_date_as_string(struct cpp *this,
   time(&tmp2);
   buf=ctime(&tmp2);
 
-  PUSH_STRING0((p_wchar0 *)buf+19, 5, tmp);
   PUSH_STRING0((p_wchar0 *)buf+4, 6, tmp);
+  PUSH_STRING0((p_wchar0 *)buf+19, 5, tmp);
 }
 
 static void check_defined(struct cpp *this,
