4b625cc16513578d9f40df0e18f90a8994fd0b79
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-03-10 01:23:19 +0000

    Fixed bug with recursive value-casting.

--- src/opcodes.c
+++ src/opcodes.c
@@ -459,7 +459,7 @@ void o_cast(struct pike_string *type, INT32 run_time_type)
       struct pike_string *itype;
       INT32 run_time_itype;
 
-      push_string(itype=index_type(type,mixed_type_string,0));
+      push_string(itype=index_type(type,int_type_string,0));
       run_time_itype=compile_type_to_runtime_type(itype);
 
       if(run_time_itype != T_MIXED)
