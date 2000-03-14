400d205f26f51113accef582cf8633447926df46
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2000-03-14 00:28:05 +0000

    Fixed type for find_autocrop().

--- src/modules/Image/image.c
+++ src/modules/Image/image.c
@@ -3998,7 +3998,7 @@ void init_image_image(void)
    ADD_FUNCTION("autocrop",image_autocrop,
 		tFuncV(tNone,tOr(tVoid,tArr(tInt)),tObj),0);
    ADD_FUNCTION("find_autocrop",image_find_autocrop,
-		tFuncV(tNone,tOr(tVoid,tArr(tInt)),tObj),0);
+		tFuncV(tNone,tOr(tVoid,tArr(tInt)),tArr(tInt)),0);
    ADD_FUNCTION("scale",image_scale,
 		tFunc(tOr(tInt,tFlt) tOr3(tInt,tFlt,tVoid),tObj),0);
    ADD_FUNCTION("translate",image_translate,
