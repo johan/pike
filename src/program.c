98097db1e46ea002a2d0f28086bb6e0bf55d273d
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2000-03-25 21:34:31 +0000

    fix for using -d3 with dumped modules

--- src/program.c
+++ src/program.c
@@ -1057,8 +1057,8 @@ void check_program(struct program *p)
   {
     if(!p->inherits[e].prog) 
     {
-      /* This inherit is not yet initialized, ignore it */
-      continue;
+      /* This inherit is not yet initialized, ignore rest of tests.. */
+      return;
     }
 
     if(p->inherits[e].storage_offset < 0)
