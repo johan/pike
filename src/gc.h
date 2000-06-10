cdf4664b99503c4339ccf261b15d8f317800d0b0
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2000-06-10 18:09:18 +0000

    GC fix.

--- src/gc.h
+++ src/gc.h
@@ -91,6 +91,7 @@ struct marker
 #define GC_DO_FREE		0x1000
 #define GC_IS_ONLY_WEAK		0x2000
 #define GC_GOT_EXTRA_REF	0x4000
+#define GC_FOLLOWED_NONSTRONG	0x8000
 
 #include "block_alloc_h.h"
 PTR_HASH_ALLOC(marker,MARKER_CHUNK_SIZE)
@@ -212,7 +213,8 @@ extern int gc_in_cycle_check;
 
 /* Use WEAK < 0 for strong links. The gc makes these assumptions about
  * those:
- * 1.  All strong links are recursed before any other links.
+ * 1.  All strong links are recursed before any other links, i.e.
+ *     strong links should be pushed last into the lifo queue.
  * 2.  There can never be a cycle consisting of only strong links.
  */
 
--- src/object.c
+++ src/object.c
@@ -1244,9 +1244,6 @@ static void low_gc_cycle_check_object(struct object *o)
     if (!o2) fatal("Object not on gc_internal_object list.\n");
 #endif
 
-    if(o->parent)
-      gc_cycle_check_object_strong(o->parent);
-
     LOW_PUSH_FRAME(o);
 
     for(e=p->num_inherits-1; e>=0; e--)
@@ -1284,6 +1281,10 @@ static void low_gc_cycle_check_object(struct object *o)
     }
     
     LOW_POP_FRAME();
+
+    /* This must be last. */
+    if(o->parent)
+      gc_cycle_check_object_strong(o->parent);
   }
 }
 
