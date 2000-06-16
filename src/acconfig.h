a539816c3bd7f0a37de990714cd3d217059dfd94
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-06-16 17:52:46 +0000

    Added USE_{,XLINKER_}YP_ .

--- src/acconfig.h
+++ src/acconfig.h
@@ -37,11 +37,17 @@
 /* Define this if your ld sets the run path with -R */
 #undef USE_R
 
+/* Define this if your ld sets the run path with -YP, */
+#undef USE_YP_
+
 /* Define this if your ld uses -rpath, but your cc wants -Wl,-rpath, */
 #undef USE_Wl
 
 /* Define this if your ld uses -R, but your cc wants -Wl,-R */
 #undef USE_Wl_R
+
+/* Define this if your ld uses -YP, , but your cc wants -Xlinker -YP, */
+#undef USE_XLINKER_YP_
 
 /* Define this if your ld doesn't have an option to set the run path */
 #undef USE_LD_LIBRARY_PATH
