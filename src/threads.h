44e3586916c633670e30a8f6ab951deacba00a5b
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-08-07 18:44:19 +0000

    Workaround for obscure gcc bug when compiling on IA64 + dmalloc.

--- src/threads.h
+++ src/threads.h
@@ -505,10 +505,15 @@ extern int Pike_in_gc;
 #define THREADS_ALLOW_UID() do { \
      struct thread_state *_tmp_uid=OBJ2THREAD(Pike_interpreter.thread_id); \
      DO_IF_DEBUG({ \
-       if(thread_for_id(th_self()) != Pike_interpreter.thread_id) \
-	 fatal("thread_for_id() (or Pike_interpreter.thread_id) failed! %p != %p\n",thread_for_id(th_self()),Pike_interpreter.thread_id); \
-       if (Pike_in_gc > 50 && Pike_in_gc < 300) \
-	 fatal("Threads allowed during garbage collection.\n"); \
+       if(thread_for_id(th_self()) != Pike_interpreter.thread_id) { \
+	 fatal("thread_for_id() (or Pike_interpreter.thread_id) failed! %p != %p\n", \
+               thread_for_id(th_self()),Pike_interpreter.thread_id); \
+       } \
+       if ((Pike_in_gc > 50) && (Pike_in_gc < 300)) { \
+         fprintf(stderr, __FILE__ ":" DEFINETOSTR(__LINE__) ": Fatal error:\n"); \
+	 debug_fatal("Threads allowed during garbage collection (%d).\n", \
+                     Pike_in_gc); \
+       } \
      }) \
      if(num_threads > 1 && !threads_disabled) { \
        SWAP_OUT_THREAD(_tmp_uid); \
