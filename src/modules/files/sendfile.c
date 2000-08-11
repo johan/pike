24bbf1c27cc8c6462a3fe1aaabc67726be1ffab0
Per Hedbor <per@pike.ida.liu.se>
2000-08-11 23:49:54 +0000

    Do not use mmap

--- src/modules/files/sendfile.c
+++ src/modules/files/sendfile.c
@@ -53,19 +53,19 @@
 #include <sys/uio.h>
 #endif /* HAVE_SYS_UIO_H */
 
+#if 0
 #ifdef HAVE_SYS_MMAN_H
 #include <sys/mman.h>
 #else /* !HAVE_SYS_MMAN_H */
 #ifdef HAVE_LINUX_MMAN_H
 #include <linux/mman.h>
 #else /* !HAVE_LINUX_MMAN_H */
-#ifdef HAVE_MMAP
-/* sys/mman.h is _probably_ there anyway. */
+#ifdef HAVE_MMAP /* sys/mman.h is _probably_ there anyway. */
 #include <sys/mman.h>
 #endif /* HAVE_MMAP */
 #endif /* HAVE_LINUX_MMAN_H */
 #endif /* HAVE_SYS_MMAN_H */
-
+#endif
 
 /* #define SF_DEBUG */
 
@@ -411,6 +411,7 @@ void low_do_sendfile(struct pike_sendfile *this)
 #endif /* HAVE_SENDFILE && !HAVE_FREEBSD_SENDFILE && !HAVE_HPUX_SENDFILE */
     SF_DFPRINTF((stderr, "sendfile: Sending file by hand\n"));
 
+#if 0 /* mmap is slower than read/write on most if not all systems */
 #if defined(HAVE_MMAP) && defined(HAVE_MUNMAP)
     {
       struct stat st;
@@ -478,7 +479,7 @@ void low_do_sendfile(struct pike_sendfile *this)
     }
   use_read_write:
 #endif /* HAVE_MMAP && HAVE_MUNMAP */
-
+#endif
     SF_DFPRINTF((stderr, "sendfile: Using read() and write().\n"));
 
     fd_lseek(this->from_fd, this->offset, SEEK_SET);
