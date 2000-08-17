32874c13b091da3d0e657e1a2e258373e0e35a3a
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-08-17 15:41:30 +0000

    Fixed bug "zero extending result of unary operation", which caused a semi-infinite loop on 64bit architectures.

--- src/modules/Gz/zlibmod.c
+++ src/modules/Gz/zlibmod.c
@@ -119,7 +119,7 @@ static int do_deflate(dynamic_buffer *buf,
 	 THREADS_DISALLOW();
 
 	 /* Absorb any unused space /Hubbe */
-	 low_make_buf_space(-this->gz.avail_out,buf);
+	 low_make_buf_space(-((ptrdiff_t)this->gz.avail_out), buf);
 
 	 /* we don't care about Z_BUF_ERROR here; it won't happen. */
       }
@@ -269,7 +269,7 @@ static int do_inflate(dynamic_buffer *buf,
       this->gz.avail_out=BUF;
       ret=inflate(& this->gz, flush);
       THREADS_DISALLOW();
-      low_make_buf_space(-this->gz.avail_out,buf);
+      low_make_buf_space(-((ptrdiff_t)this->gz.avail_out), buf);
       if(ret != Z_OK)
       {
 	fail=ret;
