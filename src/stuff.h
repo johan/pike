7e8d1bd76fc3eef9a817259abdc0911047ebe151
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2000-09-20 02:04:26 +0000

    added my_sqrt

--- src/stuff.h
+++ src/stuff.h
@@ -17,6 +17,7 @@ int my_log2(size_t x);
 int count_bits(unsigned INT32 x);
 int is_more_than_one_bit(unsigned INT32 x);
 double my_strtod(char *nptr, char **endptr);
+PMOD_EXPORT unsigned INT32 my_sqrt(unsigned INT32 n);
 /* Prototypes end here */
 
 extern INT32 hashprimes[32];
