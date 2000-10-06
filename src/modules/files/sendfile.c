9170237ca502e37e511dcc92216447ea93339b5d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-10-06 21:11:24 +0000

    Bugfix.

--- src/modules/files/sendfile.c
+++ src/modules/files/sendfile.c
@@ -855,7 +855,7 @@ static void sf_create(INT32 args)
   if ((sf.trailers) && (sf.trailers->refs > 1)) {
     struct array *a = copy_array(sf.trailers);
 #ifdef PIKE_DEBUG
-    if ((sp[4-args].type != T_ARRAY) || (sp[4-args].u.array != sf.headers)) {
+    if ((sp[4-args].type != T_ARRAY) || (sp[4-args].u.array != sf.trailers)) {
       fatal("sendfile: Stack out of sync(4).\n");
     }
 #endif /* PIKE_DEBUG */
