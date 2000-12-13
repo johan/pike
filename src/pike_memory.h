3240feae052436743f8ceb97e7f6edf14607fd10
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2000-12-13 21:31:53 +0000

    xfree - the last resort for dynamic linking on Win32?

--- src/pike_memory.h
+++ src/pike_memory.h
@@ -109,6 +109,10 @@ PMOD_EXPORT void memfill(char *to,
 	     INT32 fromlen,
 	     INT32 offset);
 PMOD_EXPORT char *debug_xalloc(size_t size);
+PMOD_EXPORT void *debug_xmalloc(size_t s);
+PMOD_EXPORT void debug_xfree(void *mem);
+PMOD_EXPORT void *debug_xrealloc(void *m, size_t s);
+PMOD_EXPORT void *debug_xcalloc(size_t n, size_t s);
 
 #undef BLOCK_ALLOC
 
