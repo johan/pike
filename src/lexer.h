fb5a9edd257cbed246ca85835509615f6584734f
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-01-15 18:24:10 +0000

    Improved handling of zero-length character constants.

--- src/lexer.h
+++ src/lexer.h
@@ -624,8 +624,14 @@ static int low_yylex(YYSTYPE *yylval)
 	yyerror("Unexpected end of file\n");
 	break;
 
-	case '\\':
-	  c = char_const();
+      case '\\':
+	c = char_const();
+	break;
+
+      case '\'':
+	yyerror("Zero-length character constant.");
+	debug_malloc_pass( yylval->n=mkintnode(0) );
+	return TOK_NUMBER;
       }
       if(!GOBBLE('\''))
 	yyerror("Unterminated character constant.");
