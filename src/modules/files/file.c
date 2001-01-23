c266d032ab1be23baae0aaf54a98c5ea16924106
Xavier Beaudouin <kiwi@pike.ida.liu.se>
2001-01-23 13:47:54 +0000

    Fixed C++-style comment. Thanks to Xavier Beaudouin <kiwi@oav.net>.

--- src/modules/files/file.c
+++ src/modules/files/file.c
@@ -264,11 +264,14 @@ static void just_close_fd(void)
 	  push_int(errno);
 	  f_strerror(1);
 	  Pike_error("Failed to close file: %s\n", Pike_sp[-1].u.string->str);
+	  break;
 
 	case EBADF:
 	  Pike_error("Internal error: Closing a non-active file descriptor %d.\n",fd);
+	  break;
+
 #ifdef SOLARIS
-       // it's actually OK. This is a bug in Solaris 8.
+	  /* It's actually OK. This is a bug in Solaris 8. */
        case EAGAIN:
          break;
 #endif
