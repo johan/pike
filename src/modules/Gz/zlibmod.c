b22a701af3c776abc3146488cc28b1060a77cab1
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2001-02-08 19:06:18 +0000

    bug workaround

--- src/modules/Gz/zlibmod.c
+++ src/modules/Gz/zlibmod.c
@@ -338,6 +338,10 @@ static int do_inflate(dynamic_buffer *buf,
       ret=inflate(& this->gz, flush);
       THREADS_DISALLOW();
       low_make_buf_space(-((ptrdiff_t)this->gz.avail_out), buf);
+
+      /* BUG WORKAROUND -Hubbe */
+      if(ret == Z_BUF_ERROR) ret=Z_OK;
+
       if(ret != Z_OK)
       {
 	fail=ret;
@@ -382,7 +386,7 @@ static void gz_inflate(INT32 args)
   initialize_buf(&buf);
 
   SET_ONERROR(err,toss_buffer,&buf);
-  fail=do_inflate(&buf,this,Z_PARTIAL_FLUSH);
+  fail=do_inflate(&buf,this,Z_SYNC_FLUSH);
   UNSET_ONERROR(err);
 
   if(fail != Z_OK && fail != Z_STREAM_END)
