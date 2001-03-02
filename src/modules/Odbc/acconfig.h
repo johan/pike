45cdb060bf91f564ddd0a6ef7307b08036f61321
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-02 14:08:01 +0000

    Support for --with-root. Added checking for various OBC typedefs.

--- src/modules/Odbc/acconfig.h
+++ src/modules/Odbc/acconfig.h
@@ -10,9 +10,28 @@
 #define PIKE_ODBC_CONFIG_H
 
 @TOP@
-@BOTTOM@
+
+/* Define if you have SQLSMALLINT */
+#undef HAVE_SQLSMALLINT
+
+/* Define if you have SQLUSMALLINT */
+#undef HAVE_SQLUSMALLINT
+
+/* Define if you have SQLINTEGER */
+#undef HAVE_SQLINTEGER
+
+/* Define if you have SQLUINTEGER */
+#undef HAVE_SQLUINTEGER
+
+/* Define if you have SQLLEN */
+#undef HAVE_SQLLEN
+
+/* Define if you have SQLULEN */
+#undef HAVE_SQLULEN
 
 /* Define if you have ODBC */
 #undef HAVE_ODBC
+
+@BOTTOM@
 
 #endif /* PIKE_ODBC_CONFIG_H */
