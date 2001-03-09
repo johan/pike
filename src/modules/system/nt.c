78093ebaac5b4d4b0f3f5ee1838f1b49ea547e83
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-09 15:12:53 +0000

    Support for detection of a few more porcessor architectures in uname(): SHx, arm, ia64, alpha64, msil.

--- src/modules/system/nt.c
+++ src/modules/system/nt.c
@@ -2312,6 +2312,49 @@ static void f_nt_uname(INT32 args)
       push_text("PPC");
       break;
 
+#ifdef PROCESSOR_ARCHITECTURE_SHX
+    case PROCESSOR_ARCHITECTURE_SHX:
+      switch (sysinfo.dwProcessorType) {
+      case PROCESSOR_HITACHI_SH3:
+        push_text("SH3");
+	break;
+      case PROCESSOR_HITACHI_SH3E:
+        push_text("SH3e");
+	break;
+      case PROCESSOR_HITACHI_SH4:
+        push_text("SH4");
+	break;
+      default:
+	push_text("SHX");
+	break;
+      }
+      break;
+#endif /* PROCESSOR_ARCHITECTURE_SHX */
+
+#ifdef PROCESSOR_ARCHITECTURE_ARM
+    case PROCESSOR_ARCHITECTURE_ARM:
+      push_text("arm");
+      break;
+#endif /* PROCESSOR_ARCHITECTURE_ARM */
+
+#ifdef PROCESSOR_ARCHITECTURE_IA64
+    case PROCESSOR_ARCHITECTURE_IA64:
+      push_text("ia64");
+      break;
+#endif /* PROCESSOR_ARCHITECTURE_IA64 */
+
+#ifdef PROCESSOR_ARCHITECTURE_ALPHA64
+    case PROCESSOR_ARCHITECTURE_ALPHA64:
+      push_text("alpha64");
+      break;
+#endif /* PROCESSOR_ARCHITECTURE_ALPHA64 */
+
+#ifdef PROCESSOR_ARCHITECTURE_MSIL
+    case PROCESSOR_ARCHITECTURE_MSIL:
+      push_text("msil");
+      break;
+#endif /* PROCESSOR_ARCHITECTURE_MSIL */
+
     default:
     case PROCESSOR_ARCHITECTURE_UNKNOWN:
       push_text("unknown");
