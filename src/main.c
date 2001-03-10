3d1ff583acd272ef93bf1e14d64d6005520d3e8c
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-10 22:26:02 +0000

    Look up the correct registry key.

--- src/main.c
+++ src/main.c
@@ -114,7 +114,7 @@ static void get_master_key(long cat)
   DWORD len=sizeof(buffer)-1,type=REG_SZ;
   long ret;
   if(RegOpenKeyEx(cat,
-		  (LPCTSTR)"SOFTWARE\\Idonex\\Pike\\0.7",
+		  (LPCTSTR)"SOFTWARE\\Idonex\\Pike\\7.0",
 		  0,KEY_READ,&k)==ERROR_SUCCESS)
   {
     if(RegQueryValueEx(k,
