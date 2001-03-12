03db0372e6ab27d827b7588bfb122095a1c14811
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2001-03-12 22:42:19 +0000

    preparation for adding more event callbacks for C programs, also saves a little memory

--- src/encode.c
+++ src/encode.c
@@ -744,8 +744,7 @@ static void encode_value2(struct svalue *val, struct encode_data *data)
       {
 	INT32 e;
 	struct program *p=val->u.program;
-	if(p->init || p->exit || p->gc_recurse_func || p->gc_check_func ||
-	   (p->flags & PROGRAM_HAS_C_METHODS))
+	if(p->event_handler || (p->flags & PROGRAM_HAS_C_METHODS))
 	  Pike_error("Cannot encode C programs.\n");
 	code_entry(type_to_tag(val->type), 1,data);
 	f_version(0);
