5fd80531d8f2222110eb2775cf62051d3520fcd5
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-17 16:36:04 +0000

    AutoDoc mk II markup fix.

--- src/program.c
+++ src/program.c
@@ -566,7 +566,7 @@ static char *raw_lfun_types[] = {
  *! Iterator creation callback.
  *!
  *! Should return an object that implements the iterator API:
- *! @ul
+ *! @dl
  *!   @item
  *!     @[lfun::`!()] should return @tt{1@} when not at end of stream,
  *!     and @tt{0@} (zero) at end of stream.
@@ -576,7 +576,7 @@ static char *raw_lfun_types[] = {
  *!     @[index()] should return the current index.
  *!   @item
  *!     @[value()] should return the current value.
- *! @endul
+ *! @enddl
  *!
  *! @seealso
  *!   @[Iterator], @[foreach()]
