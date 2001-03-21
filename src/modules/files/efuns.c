0fd4b65fee5e9023a411c89bbf80217471ced059
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2001-03-21 18:09:20 +0000

    Make rm() more unix-like on NT: It now tries to chmod a file if it can't be removed due to lack of write permission on the file itself.

--- src/modules/files/efuns.c
+++ src/modules/files/efuns.c
@@ -587,6 +587,28 @@ void f_rm(INT32 args)
     }else{
       i=unlink(str->str) != -1;
     }
+#ifdef __NT__
+    /* NT looks at the permissions on the file itself and refuses to
+     * remove files we don't have write access to. Thus we chmod it
+     * and try again, to make rm() more unix-like. */
+    if (!i && errno == EACCES && !(st.st_mode & _S_IWRITE)) {
+      if (chmod(s, st.st_mode | _S_IWRITE) == -1)
+	errno = EACCES;
+      else {
+	if(S_IFDIR == (S_IFMT & st.st_mode))
+	{
+	  i=rmdir(s) != -1;
+	}else{
+	  i=unlink(s) != -1;
+	}
+	if (!i) {		/* Failed anyway; try to restore the old mode. */
+	  int olderrno = errno;
+	  chmod(s, st.st_mode);
+	  errno = olderrno;
+	}
+      }
+    }
+#endif
   }
   THREADS_DISALLOW_UID();
       
