91751818a360ca15fd22775df26f826fbd2870b2
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-31 15:36:03 +0000

    Now detects casting of builtin functions. Fixes [bug 1373].

--- src/opcodes.c
+++ src/opcodes.c
@@ -423,7 +423,11 @@ void o_cast(struct pike_type *type, INT32 run_time_type)
 	    return;
 	    
 	  case T_FUNCTION:
-	    sp[-1].type = T_OBJECT;
+	    if (Pike_sp[-1].subtype == FUNCTION_BUILTIN) {
+	      Pike_error("Cannot cast builtin functions to object.\n");
+	    } else {
+	      sp[-1].type = T_OBJECT;
+	    }
 	    break;
 
 	  default:
