1ca35e5c915cb00e21b3f87635c37328ffbf55f4
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-04-19 14:42:06 +0000

    Fixed AutoDoc mk II markup typo.

--- src/modules/spider/spider.c
+++ src/modules/spider/spider.c
@@ -218,7 +218,7 @@ void f_parse_html(INT32 args)
 }
 
 
-/*! @decl string parse_html(string html, @
+/*! @decl string parse_html_lines(string html, @
  *!                         mapping(string:function(string, mapping(string:string), int, mixed ...:string|array)) tag_callbacks, @
  *!                         mapping(string:function(string, mapping(string:string), string, int, mixed ...:string|array)) container_callbacks, @
  *!                         mixed ... extras)
