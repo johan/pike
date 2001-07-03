8686aa00f90d36d7ffd15cfaed758e27eff57c6e
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2001-07-03 21:02:02 +0000

    fixed a memory leak

--- src/post_modules/Unicode/normalize.c
+++ src/post_modules/Unicode/normalize.c
@@ -320,6 +320,12 @@ struct pike_string *unicode_normalize( struct pike_string *source,
 {
   struct pike_string *d = unicode_decompose( source, how );
   if( how & COMPOSE_BIT )
-    d = unicode_compose( d, how );
+  {
+    struct pike_string *d2;
+    debug_malloc_touch(d2);
+    d2 = unicode_compose( d, how );
+    free_string(d);
+    return d2;
+  }
   return d;
 }
