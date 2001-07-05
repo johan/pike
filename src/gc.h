b461a45c43844d9bd25c1e6432f6189cf69c2ce5
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2001-07-05 01:54:57 +0000

    Fixed gc bug when things in partly weak mappings were freed in the zap weak pass. Probably fixes [bug 1825].

--- src/gc.h
+++ src/gc.h
@@ -178,7 +178,7 @@ int debug_gc_is_referenced(void *a);
 int gc_external_mark3(void *a, void *in, char *where);
 void debug_really_free_gc_frame(struct gc_frame *l);
 int gc_do_weak_free(void *a);
-void gc_delayed_free(void *a);
+void gc_delayed_free(void *a, TYPE_T type);
 int gc_mark(void *a);
 PMOD_EXPORT void gc_cycle_enqueue(gc_cycle_check_cb *checkfn, void *data, int weak);
 void gc_cycle_run_queue();
