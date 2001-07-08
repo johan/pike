c81c5d81cfd0bf129537ce521b40c738a58ccae7
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-07-08 20:47:07 +0000

    Begun implementing generation of computed goto code.

--- src/program_areas.h
+++ src/program_areas.h
@@ -2,7 +2,7 @@
 /* Who needs templates anyway? / Hubbe */
 
 /* Program *must* be first! */
-FOO(size_t,unsigned char,program)
+FOO(size_t,PIKE_OPCODE_T,program)
 FOO(size_t,char,linenumbers)
 FOO(unsigned INT16,unsigned INT16,identifier_index)
 FOO(unsigned INT16,unsigned INT16,variable_index)
