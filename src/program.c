8e29ca1a4dbc94a323f1315b4d8c6cfc784edfb5
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-07-08 21:00:53 +0000

    More computed goto changes.

--- src/program.c
+++ src/program.c
@@ -2079,10 +2079,15 @@ static void add_compat_event_handler(void)
     unsigned int e,d;
     unsigned char *tmp=(unsigned char *)&Pike_compiler->new_program->event_handler;
 
-    for(d=0;d<NUM_PROG_EVENTS;d++)
+    for(d=0;d<NUM_PROG_EVENTS;d++) {
+      /* FIXME: This looks like it might be broken. */
+#ifdef HAVE_COMPUTED_GOTO
+      add_to_program(Pike_compiler->new_program->event_handler);
+#else /* !HAVE_COMPUTED_GOTO */
       for(e=0;e<sizeof(Pike_compiler->new_program->event_handler);e++)
 	add_to_program(tmp[e]);
-    
+#endif /* HAVE_COMPUTED_GOTO */
+    }    
     Pike_compiler->new_program->event_handler=compat_event_handler;
   }
 }
