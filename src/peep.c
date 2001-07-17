595e155f13a531bb6d14942bbc644e8596049269
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-07-17 20:16:40 +0000

    Bugfix for sparc.

--- src/peep.c
+++ src/peep.c
@@ -200,7 +200,7 @@ void update_arg(int instr,INT32 arg)
 #define CALL_ABSOLUTE(X) do {				\
     SET_REG(REG_O2, (INT32)(X));			\
     /* jmpl %o2, %o7	*/				\
-    PUSH_INT(0x81c00000|(REG_O7<<24)|(REG_O2<<14));	\
+    PUSH_INT(0x81c00000|(REG_O7<<25)|(REG_O2<<14));	\
     /* noop		*/				\
     PUSH_INT(0x01000000);				\
   } while(0)
