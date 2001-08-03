09e1aa65235c0ecd05d6a1dc57bc5ae4d567d5ac
Build system <distmaker>
2001-08-03 09:44:23 +0000

    release number bumped to 122 by export.pike

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 2
-#define PIKE_BUILD_VERSION 121
+#define PIKE_BUILD_VERSION 122
 
 /* Prototypes begin here */
 void f_version(INT32 args);
