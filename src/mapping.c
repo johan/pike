cc68b9e335aa0283e9b7bdeb5059887ece612cd6
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2001-08-09 18:53:04 +0000

    Fixed bug causing infinite recursion when a recursive copy of a self-referencing mapping is done.

--- src/mapping.c
+++ src/mapping.c
@@ -1808,9 +1808,9 @@ PMOD_EXPORT struct mapping *copy_mapping_recursively(struct mapping *m,
   add_ref(md);
   NEW_MAPPING_LOOP(md)
   {
-    copy_svalues_recursively_no_free(sp,&k->ind, 1, p);
+    copy_svalues_recursively_no_free(sp,&k->ind, 1, &doing);
     sp++;
-    copy_svalues_recursively_no_free(sp,&k->val, 1, p);
+    copy_svalues_recursively_no_free(sp,&k->val, 1, &doing);
     sp++;
     
     mapping_insert(ret, sp-2, sp-1);
