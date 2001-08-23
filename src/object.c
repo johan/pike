2c074377b30e16f150c1f27f235124859893750d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-08-23 13:41:54 +0000

    Clear destruct_object_evaluator_callback on exit.

--- src/object.c
+++ src/object.c
@@ -1775,6 +1775,11 @@ void init_object(void)
 
 void exit_object(void)
 {
+  if (destruct_object_evaluator_callback) {
+    remove_callback(destruct_object_evaluator_callback);
+    destruct_object_evaluator_callback = NULL;
+  }
+
   if(magic_index_program)
   {
     free_program(magic_index_program);
