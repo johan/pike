be5b44c5b380e70cd437ee059b486db035a36c9b
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2001-09-01 09:38:56 +0000

    more debug

--- src/mapping.c
+++ src/mapping.c
@@ -1974,7 +1974,7 @@ void check_mapping(struct mapping *m)
     fatal("Pretty mean hashtable there buster!\n");
 
   if(md->hashsize > md->num_keypairs)
-    fatal("Pretty mean hashtable there buster (2)!\n");
+    fatal("Pretty mean hashtable there buster %d > %d (2)!\n",md->hashsize,md->num_keypairs);
 
   if(md->num_keypairs > (md->hashsize + 3) * AVG_LINK_LENGTH)
     fatal("Mapping from hell detected, attempting to send it back...\n");
