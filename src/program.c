40c560da1518f1a7d37efe310acc153661e0fff6
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-09-02 14:25:20 +0000

    Fix for [bug 2119].

--- src/program.c
+++ src/program.c
@@ -4682,6 +4682,17 @@ static void apply_trampoline(INT32 args)
   Pike_error("Internal error: Trampoline magic failed!\n");
 }
 
+static void not_trampoline(INT32 args)
+{
+  pop_n_elems(args);
+  if (!THIS->frame || !THIS->frame->current_object ||
+      !THIS->frame->current_object->prog) {
+    push_int(1);
+  } else {
+    push_int(0);
+  }
+}
+
 static void init_trampoline(struct object *o)
 {
   THIS->frame=0;
@@ -4773,6 +4784,7 @@ void init_program(void)
   debug_malloc_touch(Pike_compiler->fake_object->storage);
   ADD_STORAGE(struct pike_trampoline);
   add_function("`()",apply_trampoline,"function(mixed...:mixed)",0);
+  add_function("`!",not_trampoline,"function(:int)",0);
   set_init_callback(init_trampoline);
   set_exit_callback(exit_trampoline);
   set_gc_check_callback(gc_check_trampoline);
