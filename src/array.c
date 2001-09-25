89ed1218488c1e14e8cee2cecf600b20ece80988
Fredrik Hübinette (Hubbe) <hubbe@pike.ida.liu.se>
2001-09-25 05:55:13 +0000

    fix for [bug 1858] (this_object() in constants)

--- src/array.c
+++ src/array.c
@@ -1582,31 +1582,14 @@ PMOD_EXPORT struct array *and_arrays(struct array *a, struct array *b)
   }
 }
 
-int check_that_array_is_constant(struct array *a)
+int array_is_constant(struct array *a,
+		      struct processing *p)
 {
   array_fix_type_field(a);
-  if(a->type_field & (BIT_FUNCTION | BIT_OBJECT))
-  {
-    int e;
-    for(e=0;e<a->size;e++)
-    {
-      switch(ITEM(a)[e].type)
-      {
-	case T_FUNCTION:
-	  if(ITEM(a)[e].subtype == FUNCTION_BUILTIN) continue;
-	  /* Fall through */
-	case T_OBJECT:
-	  if(ITEM(a)[e].u.object -> next == ITEM(a)[e].u.object)
-	  {
-	    /* This is a fake object used during the
-	     * compilation!
-	     */
-	    return 0;
-	  }
-      }
-    }
-  }
-  return 1;
+  return svalues_are_constant(ITEM(a),
+			      a->size,
+			      a->type_field,
+			      p);
 }
 
 node *make_node_from_array(struct array *a)
@@ -1666,7 +1649,7 @@ node *make_node_from_array(struct array *a)
 					      mksvaluenode(ITEM(a))));
   }
   
-  if(check_that_array_is_constant(a))
+  if(array_is_constant(a,0))
   {
     debug_malloc_touch(a);
     s.type=T_ARRAY;
