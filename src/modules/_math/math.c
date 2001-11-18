5035462beea4e40a2b38d6c663071b0f83efb515
Martin Nilsson <nilsson@pike.ida.liu.se>
2001-11-18 02:57:10 +0000

    Added some autodoc comments.

--- src/modules/_math/math.c
+++ src/modules/_math/math.c
@@ -257,6 +257,7 @@ void f_sqrt(INT32 args)
 /*! @decl float log(float f)
  *!
  *! Return the natural logarithm of @[f].
+ *! @code{exp( log(x) ) == x@} for x > 0.
  *!
  *! @seealso
  *!   @[pow()], @[exp()]
@@ -275,6 +276,7 @@ void f_log(INT32 args)
 /*! @decl float exp(float f)
  *!
  *! Return the natural exponential of @[f].
+ *! @code{log( exp( x ) ) == x@} as long as exp(x) doesn't overflow an int.
  *!
  *! @seealso
  *!   @[pow()], @[log()]
