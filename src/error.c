2a93c3d0d17be9274adfe54837bfa7f6177668df
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2002-03-26 19:38:26 +0000

    Don't signal a fatal in fatal_on_error if the throw severity is THROW_EXIT, since those get past normal SETJMP's.

--- src/error.c
+++ src/error.c
@@ -329,6 +329,9 @@ static void do_abort()
 
 PMOD_EXPORT void fatal_on_error(const void *msg)
 {
+  /* It's ok if we're exiting. */
+  if (throw_severity == THROW_EXIT) return;
+
 #ifdef PIKE_DEBUG
   dump_backlog();
 #endif
