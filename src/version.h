c395b27494424a674409c4c1cf8da2c0d7b7a41e
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2002-04-29 18:39:13 +0000

    release number bumped to 34 by export.pike

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 33
+#define PIKE_BUILD_VERSION 34
 
 /* Prototypes begin here */
 void f_version(INT32 args);
