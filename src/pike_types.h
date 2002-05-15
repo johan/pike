0c0acb069642cd2cbe3cf555d2d05d062c87bbcd
Henrik Grubbström (Grubba) <grubba@grubba.org>
2002-05-15 14:48:29 +0000

    Added range_type() analogous with index_type().

--- src/pike_types.h
+++ src/pike_types.h
@@ -304,6 +304,9 @@ int pike_types_le(struct pike_type *a, struct pike_type *b);
 struct pike_type *index_type(struct pike_type *type,
 			     struct pike_type *index_type,
 			     node *n);
+struct pike_type *range_type(struct pike_type *type,
+			     struct pike_type *index1_type,
+			     struct pike_type *index2_type);
 struct pike_type *array_value_type(struct pike_type *array_type);
 struct pike_type *key_type(struct pike_type *type, node *n);
 int check_indexing(struct pike_type *type,
