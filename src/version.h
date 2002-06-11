9cd8abe130a9976c0c402ab8d036e82bc0c89279
Build system <distmaker>
2002-06-11 15:02:24 +0000

    release number bumped to 371 by export.pike

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 2
-#define PIKE_BUILD_VERSION 370
+#define PIKE_BUILD_VERSION 371
 
 /* Prototypes begin here */
 void f_version(INT32 args);
