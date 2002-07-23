21ba0b579ff683fe2bfc11e1d3a7d53046e3d65e
Honza Petrous <hop@unibase.cz>
2002-07-23 12:14:25 +0000

    Forgot to check in.

---
+++ src/modules/_Ffmpeg/acconfig.h
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ *
+ * config file for Pike Ffmpeg glue module
+ *
+ * Honza Petrous, hop@unibase.cz
+ */
+
+#ifndef FFMPEG_CONFIG_H
+#define FFMPEG_CONFIG_H
+
+@TOP@
+@BOTTOM@
+
+#endif
