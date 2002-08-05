c9cf0f0b2b8e91d621022b04d9d9e4ca0d6b1a90
Xavier Beaudouin <kiwi@pike.ida.liu.se>
2002-08-05 21:09:10 +0000

    Now ffmpeg works really with old ffmpeg libs (eg >= 0.4.5)

--- src/modules/_Ffmpeg/acconfig.h
+++ src/modules/_Ffmpeg/acconfig.h
@@ -12,4 +12,10 @@
 @TOP@
 @BOTTOM@
 
+/* Define if you have the avcodec library (-lavcodec).  */
+#undef HAVE_LIBAVCODEC
+
+/* Define if you have the ffmpeg library (-lffmpeg).  */
+#undef HAVE_LIBFFMPEG
+
 #endif
