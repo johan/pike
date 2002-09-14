7680f73d2cc40ab3a91d987495fe0cf2f7541bce
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2002-09-14 01:54:59 +0000

    Don't call the evaluator callback more often just because the --with-rtldebug flag is used.

--- src/interpret_functions.h
+++ src/interpret_functions.h
@@ -139,7 +139,6 @@
 #define DO_RETURN {				\
   if(d_flag>3) do_gc();				\
   if(d_flag>4) do_debug();			\
-  check_threads_etc();				\
   DO_DUMB_RETURN;				\
 }
 #endif
@@ -1322,7 +1321,6 @@ OPCODE1_RETURN(F_RETURN_LOCAL,"return local",0,{
      */
     if(d_flag>3) do_gc();
     if(d_flag>4) do_debug();
-    check_threads_etc();
     );
   if(Pike_fp->expendible <= Pike_fp->locals + arg1)
   {
