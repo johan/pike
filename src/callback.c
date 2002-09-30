cba72d32edfd3a7e119cb39642a147f109362094
Henrik Grubbström (Grubba) <grubba@grubba.org>
2002-09-30 17:40:17 +0000

    is_in_free_list() updated to the new block_alloc.h.

--- src/callback.c
+++ src/callback.c
@@ -43,11 +43,21 @@ extern int d_flag;
 
 static int is_in_free_list(struct callback * c)
 {
-  struct callback *foo;
+  struct callback_block *bar;
   int e;
-  for(foo=free_callbacks;foo;foo=foo->next)
-    if(foo==c)
-      return 1;
+
+  if (!c) return 0;
+
+  for (bar = callback_blocks; bar; bar=bar->next) {
+    if ((bar->x <= c) && ((c - bar->x) < CALLBACK_CHUNK)) {
+      struct callback *foo;
+      for (foo = bar->free_callbacks; foo;
+	   foo = (void *)foo->BLOCK_ALLOC_NEXT) {
+	if (foo == c) return 1;
+      }
+      return 0;
+    }
+  }
 
   return 0;
 }
