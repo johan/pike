b48034f8fb7bc8d8c197e7f9f872fcb9df4318a2
Build system <distmaker>
2002-10-01 07:38:26 +0000

    release number bumped to 403 by export.pike

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 2
-#define PIKE_BUILD_VERSION 402
+#define PIKE_BUILD_VERSION 403
 
 /* Prototypes begin here */
 void f_version(INT32 args);
