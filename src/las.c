3a13bc7b01c2a5a0b5277a2ccbf8c36c63f215b6
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2002-11-19 14:00:18 +0000

    The subtype is seldom used for strings, and certainly not in mkconstantsvaluenode.

--- src/las.c
+++ src/las.c
@@ -2028,7 +2028,7 @@ node *low_mkconstantsvaluenode(struct svalue *s)
   res->token = F_CONSTANT;
   assign_svalue_no_free(& res->u.sval, s);
 #ifdef SHARED_NODES
-  if (s->type != T_INT && s->type != T_STRING && s->type != T_FUNCTION)
+  if (s->type != T_INT && s->type != T_FUNCTION)
     /* The subtype is part of the hash, so make sure it got a defined
      * value here. */
     res->u.sval.subtype = 0;
