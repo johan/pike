29a72e47eef11a497e97996c96976a50e41153f3
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-01-09 18:06:26 +0000

    ISO-C §6.3.4.

--- src/main.c
+++ src/main.c
@@ -536,10 +536,12 @@ int dbm_main(int argc, char **argv)
    */
 #if STACK_DIRECTION < 0
   /* Equvivalent with |= 0xffff */
-  Pike_interpreter.stack_top += (~((size_t)Pike_interpreter.stack_top)) & 0xffff;
+  Pike_interpreter.stack_top += (~(((char *)Pike_interpreter.stack_top)-
+				   (char *)0)) & 0xffff;
 #else /* STACK_DIRECTION >= 0 */
   /* Equvivalent with &= ~0xffff */
-  Pike_interpreter.stack_top -= ( ((size_t)Pike_interpreter.stack_top)) & 0xffff;
+  Pike_interpreter.stack_top -= (((char *)Pike_interpreter.stack_top)-
+				  (char *)0) & 0xffff;
 #endif /* STACK_DIRECTION < 0 */
 
 #ifdef PROFILING
