a2e156b8489fc250dbc27c696d68f98454a6999d
Build system <distmaker>
2003-02-18 12:13:47 +0000

    release number bumped to 459 by export.pike

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 2
-#define PIKE_BUILD_VERSION 458
+#define PIKE_BUILD_VERSION 459
 
 /* Prototypes begin here */
 void f_version(INT32 args);
