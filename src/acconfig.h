87dcf6c3bc7c91d9b7207c350cab4c9464264a43
Mirar (Pontus Hagland) <pike@sort.mirar.org>
2003-02-27 12:36:26 +0000

    ops, forgot these defines

--- src/acconfig.h
+++ src/acconfig.h
@@ -182,6 +182,8 @@
 #undef WITH_LONG_LONG_INT
 #undef WITH_SHORT_INT
 #undef WITH_INT_INT
+#undef WITH_UNSIGNED_LONG_INT
+#undef WITH_UNSIGNED_LONG_LONG_INT
 
 /* define to the type of pike primitive ints */
 #undef INT_TYPE
