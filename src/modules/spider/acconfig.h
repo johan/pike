358148909d8ffc4ef72cdb5862861a37a681b213
Martin Nilsson <nilsson@pike.ida.liu.se>
2003-03-13 00:44:20 +0000

    This looks like useless stuff, but then I know little about these things...

--- src/modules/spider/acconfig.h
+++ src/modules/spider/acconfig.h
@@ -5,11 +5,5 @@
 || $Id$
 */
 
-/* Define this if you have an integer timezone. */
-#undef HAVE_INT_TIMEZONE
-
 /* If you do not have an 'time_t', define this to 'int' */
 #undef time_t
-
-/* Define this to enable the streamed parser module */
-#undef ENABLE_STREAMED_PARSER
