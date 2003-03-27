ce82b72435d8ab7045a13ae45af43d4638417e5d
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2003-03-27 00:48:15 +0000

    Another attempt. (Test compiling through xenofarm is a bit cumbersome..)

--- src/fdlib.h
+++ src/fdlib.h
@@ -63,7 +63,7 @@
 #include <winbase.h>
 
 typedef int FD;
-typedef struct __stat64 PIKE_STAT_T;
+typedef struct _stat64 PIKE_STAT_T;
 
 #define SOCKFUN1(NAME,T1) PMOD_EXPORT int PIKE_CONCAT(debug_fd_,NAME) (FD,T1);
 #define SOCKFUN2(NAME,T1,T2) PMOD_EXPORT int PIKE_CONCAT(debug_fd_,NAME) (FD,T1,T2);
