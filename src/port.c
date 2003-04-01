a6a5bdc0e881a1fa31e14b3597939e6ab8c0acb8
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2003-04-01 19:03:49 +0000

    Fixed a cpp conditional nesting bug that affected the LDEXP and FREXP replacements.

--- src/port.c
+++ src/port.c
@@ -988,7 +988,9 @@ long long gethrtime()
    return now;
 }
 
-#endif
+#endif	/* OWN_GETHRTIME_RDTSC */
+
+#endif	/* OWN_GETHRTIME */
 
 #ifndef HAVE_LDEXP
 double LDEXP(double x, int exp)
@@ -1005,6 +1007,4 @@ double FREXP(double x, int *exp)
   ret = (x*pow(2.0,(double)-*exp));
   return ret;
 }
-#endif
-
 #endif
