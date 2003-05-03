ea34da18cec5bf0c6d801fc000d96e691ab651ed
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2003-05-03 17:48:22 +0000

    Fixed PIKE_FEATURE. Some cleanups.

--- src/modules/sybase/acconfig.h
+++ src/modules/sybase/acconfig.h
@@ -24,11 +24,6 @@
 #undef PIKE_HAVE_LIBCT
 #undef PIKE_HAVE_LIBINTL
 #undef PIKE_HAVE_LIBSYBTCL
-
-#if defined(PIKE_HAVE_LIBCOMN) && defined(PIKE_HAVE_LIBCS) \
-  && defined(PIKE_HAVE_LIBCT) && defined(PIKE_HAVE_LIBINTL) \
-  && defined(PIKE_HAVE_LIBSYBTCL) && defined(HAVE_CTPUBLIC_H)
-#define HAVE_SYBASE 
-#endif
+#undef HAVE_SYBASE
 
 #endif /* __PIKE_SYBASE_CONFIG_H */
