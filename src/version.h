c8842648f68a3b7fd16d4d22b71476a08c9bf1ab
Peter Bortas <peter@bortas.org>
2003-06-17 21:21:53 +0000

    release number bumped to 24 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 23
+#define PIKE_BUILD_VERSION 24
 
 /* Prototypes begin here */
 void f_version(INT32 args);
