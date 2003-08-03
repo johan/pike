7782298feb7fb48e0d6b81e944bb7fa41cd5b257
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2003-08-03 02:34:46 +0000

    Adapted this_object() to the fact that it doesn't always access the lexically surrounding parent objects.

--- src/interpret_functions.h
+++ src/interpret_functions.h
@@ -2274,15 +2274,15 @@ OPCODE0(F_BREAKPOINT, "breakpoint", 0, {
 
 OPCODE1(F_THIS_OBJECT, "this_object", 0, {
     struct object *o = Pike_fp->current_object;
-    int level = arg1;
-    for (; level > 0; level--) {
+    int level;
+    for (level = 0; level < arg1; level++) {
       struct program *p = o->prog;
       if (!p)
-	Pike_error ("Cannot get the parent object of a destructed object.\n");
-      DO_IF_DEBUG (
-	if (!(p->flags & PROGRAM_USES_PARENT))
-	  Pike_fatal ("optimize_this_object failed to set up parent pointers.\n");
-      );
+	Pike_error ("Object %d level(s) up is destructed - cannot get the parent.\n",
+		    level);
+      if (!(p->flags & PROGRAM_USES_PARENT))
+	/* FIXME: Ought to write out the object here. */
+	Pike_error ("Object %d level(s) up lacks parent reference.\n", level);
       o = PARENT_INFO(o)->parent;
     }
     ref_push_object(o);
