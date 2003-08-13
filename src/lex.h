5538bc59c3b59a480e7de46d89cd02a939184ffa
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-08-13 15:58:04 +0000

    Added I_UPDATE_* flags to allow for improved machinecode generation.

--- src/lex.h
+++ src/lex.h
@@ -41,7 +41,15 @@ struct keyword
 #define I_BRANCH	128	/* Opcode either jumps to the address
 				 * given by I_POINTER/I_HASPOINTER or
 				 * continues. */
+/* The following are useful for the code generator.
+ * Note that they apply to the change of state as seen
+ * by the immediately following instruction.
+ */
+#define I_UPDATE_SP	256	/* Opcode modifies Pike_sp */
+#define I_UPDATE_FP	512	/* Opcode modifies Pike_fp */
+#define I_UPDATE_M_SP	1024	/* Opcode modifies Pike_mark_sp */
 
+/* Convenience variants */
 #define I_TWO_ARGS	(I_HASARG | I_HASARG2)
 #define I_DATA		(I_HASARG | I__DATA)
 #define I_ISPOINTER	(I_HASARG | I_POINTER)	/* Only F_POINTER */
