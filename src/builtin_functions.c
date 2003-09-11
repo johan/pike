afc82c1b4c2fdf959bad45ca762292faad21a323
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-09-11 13:45:44 +0000

    Fixed poll(2) support for MacOS X.

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -3576,7 +3576,11 @@ PMOD_EXPORT void f_sleep(INT32 args)
 #ifdef __NT__
        Sleep(DO_NOT_WARN((int)(left*1000)));
 #elif defined(HAVE_POLL)
-       poll(NULL,0,(int)(left*1000));
+       {
+	 /* MacOS X is stupid, and requires a non-NULL pollfd pointer. */
+	 struct pollfd sentinel;
+	 poll(&sentinel, 0, (int)(left*1000));
+       }
 #else
        {
 	 struct timeval t3;
@@ -3690,7 +3694,11 @@ PMOD_EXPORT void f_delay(INT32 args)
 #ifdef __NT__
 	 Sleep(DO_NOT_WARN((int)(left*1000)));
 #elif defined(HAVE_POLL)
-	 poll(NULL,0,(int)(left*1000));
+	 {
+	   /* MacOS X is stupid, and requires a non-NULL pollfd pointer. */
+	   struct pollfd sentinel;
+	   poll(&sentinel, 0, (int)(left*1000));
+	 }
 #else
 	 {
 	   struct timeval t3;
@@ -4497,7 +4505,7 @@ static ptrdiff_t low_parse_format(p_wchar0 *s, ptrdiff_t slen)
 	case '\'':
 	case '+':
 	case '~':
-	  break;
+	  continue;
 	  /* Attributes */
 	case '.':
 	case ':':
