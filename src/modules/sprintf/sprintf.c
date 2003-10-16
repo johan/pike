ff4bc014e3379cc3913fd7fa8067047031bac66d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-10-16 16:23:21 +0000

    Fix for [bug 3528].

--- src/modules/sprintf/sprintf.c
+++ src/modules/sprintf/sprintf.c
@@ -839,8 +839,10 @@ static void low_pike_sprintf(struct string_builder *r,
       {
 	struct pike_string *s;
 	GET_STRING(s);
-	fsp->pad_string=MKPCHARP_STR(s);
-	fsp->pad_length=s->len;
+	if (s->len) {
+	  fs->fsp->pad_string=MKPCHARP_STR(s);
+	  fs->fsp->pad_length=s->len;
+	}
 	continue;
       }
 
