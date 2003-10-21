aaa600a00b15039507d67b5eff7802e7931225de
Martin Nilsson <nilsson@pike.ida.liu.se>
2003-10-21 02:43:17 +0000

    release number bumped to 33 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 32
+#define PIKE_BUILD_VERSION 33
 
 /* Prototypes begin here */
 void f_version(INT32 args);
