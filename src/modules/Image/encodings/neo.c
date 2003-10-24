551cf74e460574217b285d02f8c985fd944cb5b3
Martin Nilsson <nilsson@pike.ida.liu.se>
2003-10-24 23:36:12 +0000

    Wups.

--- src/modules/Image/encodings/neo.c
+++ src/modules/Image/encodings/neo.c
@@ -48,7 +48,7 @@ void image_neo_f__decode(INT32 args)
   struct object *img;
 
   struct pike_string *s, *fn;
-  const unsigned char *q;
+  unsigned char *q;
 
   get_all_args( "decode", args, "%S", &s );
   if(s->len!=32128)
@@ -61,7 +61,7 @@ void image_neo_f__decode(INT32 args)
     Pike_error("This is not a NEO file (invalid resolution).\n");
 
   /* Checks done... */
-  stack_pop_n_elems_keep_top(args);
+  pop_n_elems(args);
 
   if(res==0)
     pal = decode_atari_palette(q+4, 16);
