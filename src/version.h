0dacf526b592b4fdfb746ea2c1af40912f43e677
Martin Nilsson <nilsson@pike.ida.liu.se>
2004-02-25 15:43:29 +0000

    release number bumped to 44 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 43
+#define PIKE_BUILD_VERSION 44
 
 /* Prototypes begin here */
 void f_version(INT32 args);
