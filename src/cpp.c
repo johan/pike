caf514b900202b62361c5110eca0fc91e30f6fde
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2004-04-15 20:34:45 +0000

    Trivial fix.

--- src/cpp.c
+++ src/cpp.c
@@ -1534,7 +1534,7 @@ static int do_safe_index_call(struct cpp *this, struct pike_string *s)
 
   if (SETJMP_SP(recovery, 1)) {
     if (TEST_COMPAT (7, 4)) {
-      free_string (&throw_value);
+      free_svalue (&throw_value);
       throw_value.type = T_INT;
     }
     else {
