f905c1fed1084358979c69bc1e3fade68e3b6dcc
Martin Nilsson <nilsson@pike.ida.liu.se>
2004-04-18 19:32:32 +0000

    Reverted change in grey table. Image objects operates in linear color space and I have no idea where to apply what gamma. Better leave it to mirar to figure out.

--- src/modules/Image/encodings/png.c
+++ src/modules/Image/encodings/png.c
@@ -537,8 +537,8 @@ static int _png_write_rgb(rgb_group *w1,
    /* w1, wa1 will be freed upon error */
 
    static rgb_group white={255,255,255};
-   static rgb_group grey4[4]={{0,0,0},{154,154,154},
-			      {212,212,212},{255,255,255}};
+   static rgb_group grey4[4]={{0,0,0},{85,85,85},
+			      {170,170,170},{255,255,255}};
    static rgb_group black={0,0,0};
 
    rgb_group *d1=w1;
