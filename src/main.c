ab61eab7f2ee2fd7ce380254bcf03904e7f046ef
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-05-11 15:33:07 +0000

    Fixed typo in use of TRACE.

--- src/main.c
+++ src/main.c
@@ -757,7 +757,7 @@ int dbm_main(int argc, char **argv)
   }
   UNSETJMP(back);
 
-  TRACE((stderr, "Exit %s...\n", num));
+  TRACE((stderr, "Exit %d...\n", num));
   
   pike_do_exit(num);
   return num; /* avoid warning */
