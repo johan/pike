ce2117ed40398a4a03c161cbeb9fed16ddb44158
Martin Nilsson <nilsson@pike.ida.liu.se>
2004-05-19 00:13:13 +0000

    Used ATTRIBUTE const on no depend no side effect functions with no pointer arguments.

--- src/stuff.h
+++ src/stuff.h
@@ -11,12 +11,12 @@
 #include "global.h"
 
 /* Prototypes begin here */
-PMOD_EXPORT int my_log2(size_t x);
-PMOD_EXPORT int count_bits(unsigned INT32 x);
-PMOD_EXPORT int is_more_than_one_bit(unsigned INT32 x);
+PMOD_EXPORT int my_log2(size_t x) ATTRIBUTE((const));
+PMOD_EXPORT int count_bits(unsigned INT32 x) ATTRIBUTE((const));
+PMOD_EXPORT int is_more_than_one_bit(unsigned INT32 x) ATTRIBUTE((const));
 PMOD_EXPORT double my_strtod(char *nptr, char **endptr);
-PMOD_EXPORT unsigned INT32 my_sqrt(unsigned INT32 n);
-unsigned long find_good_hash_size(unsigned long x);
+PMOD_EXPORT unsigned INT32 my_sqrt(unsigned INT32 n) ATTRIBUTE((const));
+unsigned long find_good_hash_size(unsigned long x) ATTRIBUTE((const));
 /* Prototypes end here */
 
 PMOD_EXPORT extern INT32 hashprimes[32];
