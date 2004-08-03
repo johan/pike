39a0216e72005f51b4f316dda4d7ef646b0abb62
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-08-03 14:51:28 +0000

    Oops.

--- lib/modules/Graphics.pmod/Graph.pmod/graph.h
+++ lib/modules/Graphics.pmod/Graph.pmod/graph.h
@@ -11,7 +11,7 @@ private constant LITET = 1.0e-38;
 private constant STORTLITET = 1.0e-30;
 private constant STORT = 1.0e30;
 
-#define GETFONT(WHATFONT) ((diagram_data->WHATFONT) || diagram_data->font);
+#define GETFONT(WHATFONT) ((diagram_data->WHATFONT) || diagram_data->font)
 
 //#define BG_DEBUG 1
 
