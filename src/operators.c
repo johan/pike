84e6ff67aad2bbb9be48bfcc90089b164ce3470b
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-08-24 14:34:06 +0000

    Made the two argument case of generate_sum() more generic.

--- src/operators.c
+++ src/operators.c
@@ -1553,8 +1553,8 @@ static int generate_sum(node *n)
     {
       emit0(F_ADD_FLOATS);
     }
-    else if(first_arg[0]->type == int_type_string &&
-       second_arg[0]->type == int_type_string)
+    else if(pike_types_le(first_arg[0]->type, int_type_string) &&
+	    pike_types_le(second_arg[0]->type, int_type_string))
     {
       emit0(F_ADD_INTS);
     }
