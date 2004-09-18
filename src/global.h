ef2cbccd6edee6cb96c3eb4ae22eed198ce163cf
Per Hedbor <per@pike.ida.liu.se>
2004-09-18 20:17:36 +0000

    rcsid can be constant..

--- src/global.h
+++ src/global.h
@@ -362,14 +362,14 @@ typedef struct p_wchar_p
 #ifndef CONFIGURE_TEST
 #if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7)
 #define RCSID(X) \
- static char *rcsid __attribute__ ((unused)) =X
+ static const char rcsid[] __attribute__ ((unused)) =X
 #elif __GNUC__ == 2
 #define RCSID(X) \
- static char *rcsid = X; \
- static void *use_rcsid=(&use_rcsid, (void *)&rcsid)
+ static const char rcsid[] = X; \
+ static const void *use_rcsid=(&use_rcsid, (void *)&rcsid)
 #else
 #define RCSID(X) \
- static char *rcsid = X
+ static const char rcsid[] = X
 #endif
 #else
 #define RCSID(X)
