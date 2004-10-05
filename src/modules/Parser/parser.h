dc58f9b94236c5e9530db9a7bd8298edd739fe5c
Martin Nilsson <nilsson@pike.ida.liu.se>
2004-10-05 21:17:15 +0000

    More prototypes

--- src/modules/Parser/parser.h
+++ src/modules/Parser/parser.h
@@ -6,5 +6,16 @@
 */
 
 void init_parser_html(void);
+void exit_parser_html(void);
 void init_parser_rcs(void);
 void exit_parser_rcs(void);
+void init_parser_c(void);
+void exit_parser_c(void);
+void init_parser_pike(void);
+void exit_parser_pike(void);
+
+/* c.c */
+
+void push_token0( struct array **a, p_wchar0 *x, int l);
+void push_token1( struct array **a, p_wchar1 *x, int l);
+void push_token2( struct array **a, p_wchar2 *x, int l);
