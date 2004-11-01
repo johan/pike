356d75b06b171b6f959bf25e7c05d8d18ad5798a
Martin Nilsson <nilsson@pike.ida.liu.se>
2004-11-01 03:52:01 +0000

    Bugfixed my get_dir() code.

--- src/modules/files/efuns.c
+++ src/modules/files/efuns.c
@@ -807,10 +807,12 @@ void f_get_dir(INT32 args)
 
   if(!str) {
 #if defined(__amigaos4__) || defined(__NT__)
-    str = make_shared_string("");
+    push_constant_text("");
 #else
-    str = make_shared_string(".");
+    push_constant_text(".");
 #endif
+    str = Pike_sp[-1].u.string;
+    args++;
   }
 
   if (strlen(str->str) != (size_t)str->len) {
@@ -982,7 +984,8 @@ void f_get_dir(INT32 args)
     closedir(dir);
 
     END_AGGREGATE_ARRAY;
-    stack_pop_n_elems_keep_top(args);
+    if(args)
+      stack_pop_n_elems_keep_top(args);
   } else {
     pop_n_elems(args);
     push_int(0);
