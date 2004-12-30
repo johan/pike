f7487267bf190fc67afdc7691c5d17451a64d9f2
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-12-30 13:28:22 +0000

    set_close_on_exec() is now fd flag clean.

--- src/fd_control.c
+++ src/fd_control.c
@@ -185,9 +185,22 @@ PMOD_EXPORT int set_close_on_exec(int fd, int which)
 {
 #ifndef HAVE_BROKEN_F_SETFD
   int ret;
+  if (which) {
+    do {
+      which = fcntl(fd, F_GETFD);
+    } while (which < 0 && errno == EINTR);
+    if (which < 0) which = FD_CLOEXEC;
+    else which |= FD_CLOEXEC;
+  } else {
+    do {
+      which = fcntl(fd, F_GETFD);
+    } while (which < 0 && errno == EINTR);
+    if (which < 0) which = 0;
+    else which &= ~FD_CLOEXEC;
+  }
   do 
   {
-    ret=fcntl(fd, F_SETFD, which ? FD_CLOEXEC : 0);
+    ret=fcntl(fd, F_SETFD, which);
   } while (ret <0 && errno==EINTR );
   return ret;
 #else /* HAVE_BROKEN_F_SETFD */
