49844ca2e611c68023071e461aa6b11bd5397513
Martin Nilsson <nilsson@pike.ida.liu.se>
2005-02-26 01:14:23 +0000

    Fixed buffer overrun that occurs when the windows version name is longer than 1000 chars or so (i.e. never).

--- src/modules/system/nt.c
+++ src/modules/system/nt.c
@@ -3153,7 +3153,7 @@ static void f_nt_uname(INT32 args)
       break;
   }
 
-  sprintf(buf,"Windows %s %ld.%ld.%ld",
+  snprintf(buf, sizeof(buf), "Windows %s %ld.%ld.%ld",
 	  version,
 	  osversion.dwMajorVersion,
 	  osversion.dwMinorVersion,
