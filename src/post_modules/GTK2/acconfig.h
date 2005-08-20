3738e227e6d94b0aa05d4eafaa49ee8a90dc7526
Martin Nilsson <nilsson@pike.ida.liu.se>
2005-08-20 03:02:24 +0000

    Some more GTK2 files.

---
+++ src/post_modules/GTK2/acconfig.h
@@ -0,0 +1,21 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id$
+*/
+
+#undef FUNCPROTO
+#undef HAVE_GNOME
+#undef HAVE_DPMS
+#undef PGTK_AUTO_UTF8
+#undef HAVE_GTK22
+#undef HAVE_GTK24
+#undef HAVE_GTK26
+#undef HAVE_ATK
+#undef HAVE_ATK18
+#undef HAVE_PANGO
+#undef HAVE_PANGO16
+#undef HAVE_PANGO14
+#undef HAVE_PANGO12
+#undef HAVE_GNOMECANVAS
