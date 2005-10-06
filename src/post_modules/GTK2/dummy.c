ccdd4f82cd23c89171488046798b8a56b77fbc2a
Martin Nilsson <nilsson@pike.ida.liu.se>
2005-10-06 00:21:14 +0000

    Dummy module

---
+++ src/post_modules/GTK2/dummy.c
@@ -0,0 +1,22 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id$
+*/
+
+#include "global.h"
+#include "pgtk_config.h"
+#include "module.h"
+
+RCSID("$Id$");
+
+/* Well... Sort of basic, right? :-) */
+
+PIKE_MODULE_INIT
+{
+}
+
+PIKE_MODULE_EXIT
+{
+}
