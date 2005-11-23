12600e58eabc74d1f442d6c20547993490820d06
Martin Nilsson <nilsson@pike.ida.liu.se>
2005-11-23 01:10:58 +0000

    Removed noreturn attribute from pike_do_exit, as it yields insignificant optimization and causes compiler problems.

--- src/main.h
+++ src/main.h
@@ -10,7 +10,7 @@
 
 /* Prototypes begin here */
 int main(int argc, char **argv);
-DECLSPEC(noreturn) void pike_do_exit(int num) ATTRIBUTE((noreturn));
+void pike_do_exit(int num);
 /* Prototypes end here */
 
 #endif /* !MAIN_H */
