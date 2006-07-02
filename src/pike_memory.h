babc78dd324c3bd324eea88120e2d602d0980024
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2006-07-02 00:05:45 +0000

    Fixed case when valgrind is available but its header files aren't.

--- src/pike_memory.h
+++ src/pike_memory.h
@@ -13,13 +13,23 @@
 
 #ifdef USE_VALGRIND
 
+#define HAVE_VALGRIND_MACROS
+/* Assume that any of the following header files have the macros we
+ * need. Haven't checked if it's true or not. */
+
 #ifdef HAVE_MEMCHECK_H
 #include <memcheck.h>
 #elif defined(HAVE_VALGRIND_MEMCHECK_H)
 #include <valgrind/memcheck.h>
 #elif defined(HAVE_VALGRIND_H)
 #include <valgrind.h>
+#else
+#undef HAVE_VALGRIND_MACROS
 #endif
+
+#endif	/* USE_VALGRIND */
+
+#ifdef HAVE_VALGRIND_MACROS
 
 /* No Access */
 #define PIKE_MEM_NA(lvalue) do {					\
@@ -56,7 +66,7 @@
 /* Return true if a memchecker is in use. */
 #define PIKE_MEM_CHECKER() RUNNING_ON_VALGRIND
 
-#else
+#else  /* !HAVE_VALGRIND_MACROS */
 
 #define PIKE_MEM_NA(lvalue)		do {} while (0)
 #define PIKE_MEM_NA_RANGE(addr, bytes)	do {} while (0)
@@ -68,7 +78,7 @@
 #define PIKE_MEM_RO_RANGE(addr, bytes)	do {} while (0)
 #define PIKE_MEM_CHECKER()		0
 
-#endif /* USE_VALGRIND */
+#endif	/* !HAVE_VALGRIND_MACROS */
 
 
 #define MEMSEARCH_LINKS 512
