2005e37a88e00cc5a4eb5d1d98117a69d92e1300
Henrik Grubbström (Grubba) <grubba@grubba.org>
2006-08-14 14:07:23 +0000

    Backported character set handling from Pike 7.6.

--- src/modules/Mysql/acconfig.h
+++ src/modules/Mysql/acconfig.h
@@ -32,13 +32,46 @@
 /* Define if you have mysql_options(). */
 #undef HAVE_MYSQL_OPTIONS
 
+/* Define if you have mysql_set_character_set(). */
+#undef HAVE_MYSQL_SET_CHARACTER_SET
+
 /* Define if you have the mysql_port variable. */
 #undef HAVE_MYSQL_PORT
 
 /* Define if you have the mysql_unix_port variable. */
 #undef HAVE_MYSQL_UNIX_PORT
 
+/* Define if your mysql.h defines MYSQL_OPT_CONNECT_TIMEOUT */
+#undef HAVE_MYSQL_OPT_CONNECT_TIMEOUT
+
+/* Define if your mysql.h defines MYSQL_OPT_COMPRESS */
+#undef HAVE_MYSQL_OPT_COMPRESS
+
+/* Define if your mysql.h defines MYSQL_OPT_NAMED_PIPE */
+#undef HAVE_MYSQL_OPT_NAMED_PIPE
+
+/* Define if your mysql.h defines MYSQL_INIT_COMMAND */
+#undef HAVE_MYSQL_INIT_COMMAND
+
+/* Define if your mysql.h defines MYSQL_READ_DEFAULT_FILE */
+#undef HAVE_MYSQL_READ_DEFAULT_FILE
+
+/* Define if your mysql.h defines MYSQL_READ_DEFAULT_GROUP */
+#undef HAVE_MYSQL_READ_DEFAULT_GROUP
+
+/* Define if your mysql.h defines MYSQL_SET_CHARSET_DIR */
+#undef HAVE_MYSQL_SET_CHARSET_DIR
+
+/* Define if your mysql.h defines MYSQL_SET_CHARSET_NAME */
+#undef HAVE_MYSQL_SET_CHARSET_NAME
+
+/* Define if your mysql.h defines MYSQL_OPT_LOCAL_INFILE */
+#undef HAVE_MYSQL_OPT_LOCAL_INFILE
+
 /* Define if your mysql.h defines SHUTDOWN_DEFAULT */
 #undef HAVE_SHUTDOWN_DEFAULT
+
+/* Define if your mysql.h defines FIELD_TYPE_GEOMETRY */
+#undef HAVE_FIELD_TYPE_GEOMETRY
 
 #endif /* PIKE_MYSQL_CONFIG_H */
