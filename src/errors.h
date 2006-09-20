93955f7d8103d12beaf45c98d430cb960e9569eb
Martin Stjernholm <mast@lysator.liu.se>
2006-09-20 16:15:55 +0000

    Made Error.Generic a bit easier to inherit and override. Also added some doc to support error message extension by rethrowing code.

--- src/errors.h
+++ src/errors.h
@@ -8,9 +8,11 @@
 #ifdef ERR_DECLARE
 #define DECLARE_ERROR(NAME, SCNAME, INHERIT, DECL) \
 PMOD_EXPORT struct program *PIKE_CONCAT(NAME,_error_program); \
-ptrdiff_t PIKE_CONCAT(NAME,_error_offset); 
+ptrdiff_t PIKE_CONCAT(NAME,_error_offset); \
+DECL
 
-#define ERR_VAR(TYPE,CTYPE,RUNTYPE,NAME) TYPE NAME ;
+#define ERR_FUNC_SAVE_ID(VAR, NAME, FUNC, TYPE, FLAGS) static int VAR;
+
 #endif
 
 #ifdef ERR_EXT_DECLARE
@@ -56,6 +58,9 @@ struct PIKE_CONCAT(NAME,_error_struct) { \
 #define ERR_FUNC(NAME,FUNC,TYPE,FLAGS) \
   ADD_FUNCTION(NAME,FUNC,TYPE,FLAGS);
 
+#define ERR_FUNC_SAVE_ID(VAR, NAME, FUNC, TYPE, FLAGS) \
+  VAR = ADD_FUNCTION(NAME,FUNC,TYPE,FLAGS);
+
 #endif
 
 #ifdef ERR_CLEANUP
@@ -66,6 +71,9 @@ struct PIKE_CONCAT(NAME,_error_struct) { \
   }
 #endif
 
+#ifndef EMPTY
+#define EMPTY
+#endif
 
 #ifndef ERR_INHERIT
 #define ERR_INHERIT(NAME)
@@ -79,14 +87,20 @@ struct PIKE_CONCAT(NAME,_error_struct) { \
 #define ERR_FUNC(NAME,FUNC,TYPE,FLAGS)
 #endif
 
+#ifndef ERR_FUNC_SAVE_ID
+#define ERR_FUNC_SAVE_ID(VAR, NAME, FUNC, TYPE, FLAGS)
+#endif
+
 DECLARE_ERROR(generic, Generic, EMPTY ,
   ERR_VAR(struct pike_string *,tStr,PIKE_T_STRING,error_message)
   ERR_VAR(struct array *,tArray,PIKE_T_ARRAY,error_backtrace)
   ERR_FUNC("cast",f_error_cast,tFunc(tString,tArray),ID_STATIC)
   ERR_FUNC("`[]",f_error_index,tFunc(tInt01,tMixed),ID_STATIC)
   ERR_FUNC("describe",f_error_describe,tFunc(tVoid,tString),0)
-  ERR_FUNC("message", f_error_message, tFunc(tVoid,tString), 0)
-  ERR_FUNC("backtrace",f_error_backtrace,tFunc(tVoid,tArr(tMixed)),0)
+  ERR_FUNC_SAVE_ID (generic_err_message_fun, "message", f_error_message,
+		    tFunc(tVoid,tString), 0)
+  ERR_FUNC_SAVE_ID (generic_err_backtrace_fun, "backtrace", f_error_backtrace,
+		    tFunc(tVoid,tArr(tMixed)),0)
   ERR_FUNC("_sprintf", f_error__sprintf,
 	   tFunc(tOr(tInt,tVoid) tOr(tMapping,tVoid),tString), ID_STATIC)
   ERR_FUNC("_is_type", f_error__is_type, tFunc(tString, tInt01), ID_STATIC)
@@ -141,6 +155,7 @@ DECLARE_ERROR (module_load, ModuleLoad,
 #undef ERR_VAR
 #undef EMPTY
 #undef ERR_FUNC
+#undef ERR_FUNC_SAVE_ID
 
 #undef ERR_DECLARE
 #undef ERR_EXT_DECLARE
