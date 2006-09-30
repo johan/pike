6156eec2a4c8656fc0795fe2ddb1ad6e29b00dab
Build system <distmaker>
2006-09-30 08:19:57 +0000

    release number bumped to 443 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 442
+#define PIKE_BUILD_VERSION 443
 
 /* Prototypes begin here */
 void f_version(INT32 args);
