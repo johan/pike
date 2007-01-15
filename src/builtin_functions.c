5ce70e519e9109db60add0c0c82cbf839405ccd7
Henrik Grubbström (Grubba) <grubba@grubba.org>
2007-01-15 15:50:38 +0000

    Increased paranoia in efun::exit() with werror arguments. Found by Klocwork.

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -2487,9 +2487,15 @@ PMOD_EXPORT void f_exit(INT32 args)
   in_exit=1;
 
   if(args>1 && Pike_sp[1-args].type==T_STRING) {
-    apply_svalue(simple_mapping_string_lookup(get_builtin_constants(),
-					      "werror"), args-1);
-    pop_stack();
+    struct svalue *s =
+      simple_mapping_string_lookup(get_builtin_constants(), "werror");
+    if (s) {
+      apply_svalue(s, args-1);
+      pop_stack();
+    } else if (args > 1) {
+      fprintf(stderr, "No efun::werror() at exit.\n");
+      pop_n_elems(args-1);
+    }
     args=1;
   }
 
