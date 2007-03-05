518c9ec81a1fccb357d5a5a264f4c89ea0bee7d7
Henrik Grubbström (Grubba) <grubba@grubba.org>
2007-03-05 18:10:40 +0000

    Constant string nodes now get stricter types.

--- src/las.c
+++ src/las.c
@@ -1222,14 +1222,13 @@ node *debug_mkstrnode(struct pike_string *str)
 {
   node *res = mkemptynode();
   res->token = F_CONSTANT;
-  copy_pike_type(res->type, string_type_string);
   res->node_info = 0;
   res->u.sval.type = T_STRING;
 #ifdef __CHECKER__
   res->u.sval.subtype = 0;
 #endif
   copy_shared_string(res->u.sval.u.string, str);
-
+  res->type = get_type_of_svalue(&res->u.sval);
   return freeze_node(res);
 }
 
