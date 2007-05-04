2b1e99220d7c12238bcc60971ae26414d394a983
Henrik Grubbström (Grubba) <grubba@grubba.org>
2007-05-04 18:35:18 +0000

    Potential fix for [bug 4362].

--- src/las.c
+++ src/las.c
@@ -3888,13 +3888,15 @@ void fix_type_field(node *n)
       yyerror("Indexing a void expression.");
       /* The optimizer converts this to an expression returning 0. */
       copy_pike_type(n->type, zero_type_string);
-    } else {
+    } else if (CDR(n)) {
       type_a=CAR(n)->type;
       type_b=CDR(n)->type;
       if(!check_indexing(type_a, type_b, n))
 	if(!Pike_compiler->catch_level)
 	  yyerror("Indexing on illegal type.");
       n->type=index_type(type_a, type_b,n);
+    } else {
+      copy_pike_type(n->type, mixed_type_string);
     }
     break;
 
