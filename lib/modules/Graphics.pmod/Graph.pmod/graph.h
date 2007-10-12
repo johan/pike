b435f77dcd9175ee734daf7d3fce14b861073f3e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2007-10-12 15:56:04 +0000

    Fixed some warnings about unused symbols.

--- lib/modules/Graphics.pmod/Graph.pmod/graph.h
+++ lib/modules/Graphics.pmod/Graph.pmod/graph.h
@@ -7,10 +7,6 @@
 #define SEP "\t"
 #define UNICODE(TEXT,ENCODING) Locale.Charset.decoder(ENCODING)->feed(TEXT)->drain()
 
-private constant LITET = 1.0e-38;
-private constant STORTLITET = 1.0e-30;
-private constant STORT = 1.0e30;
-
 #define GETFONT(WHATFONT) ((diagram_data->WHATFONT) || diagram_data->font)
 
 //#define BG_DEBUG 1
