44f64985bed0058bb083abffe19f7858bbf90cc1
Henrik Grubbström (Grubba) <grubba@grubba.org>
2007-11-03 20:11:48 +0000

    push_int_type() now knows about T_ZERO.

--- src/pike_types.c
+++ src/pike_types.c
@@ -687,10 +687,14 @@ void debug_push_int_type(INT_TYPE min, INT_TYPE max)
 	       min, max);
 #endif /* PIKE_DEBUG */
 
-  *(++Pike_compiler->type_stackp) = mk_type(T_INT,
-					    (void *)(ptrdiff_t)min,
-					    (void *)(ptrdiff_t)max, 0);
-
+  if (!min && !max) {
+    /* Special case... */
+    push_type(T_ZERO);
+  } else {
+    *(++Pike_compiler->type_stackp) = mk_type(T_INT,
+					      (void *)(ptrdiff_t)min,
+					      (void *)(ptrdiff_t)max, 0);
+  }
   TYPE_STACK_DEBUG("push_int_type");
 }
 
