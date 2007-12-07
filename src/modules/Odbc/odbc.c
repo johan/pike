996c964774973cbb6f89fd9f09e5b97f51525a85
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2007-12-07 17:31:09 +0000

    Some cleanup of the THREADS_ALLOW/DISALLOW stuff.

--- src/modules/Odbc/odbc.c
+++ src/modules/Odbc/odbc.c
@@ -44,24 +44,6 @@ RCSID("$Id$");
 
 #ifdef HAVE_ODBC
 
-/* http://msdn2.microsoft.com/en-us/library/ms715361.aspx says:
- *
- *   On multithread operating systems, drivers must be thread-safe.
- *   That is, it must be possible for applications to use the same
- *   handle on more than one thread.
- *
- * This means we don't need a lock at all on the connection when we
- * release the interpreter lock. If this really is true remains to be
- * seen..
- */
-#ifdef PIKE_THREADS
-#define ODBC_ALLOW() THREADS_ALLOW()
-#define ODBC_DISALLOW()	THREADS_DISALLOW()
-#else
-#define ODBC_ALLOW()
-#define ODBC_DISALLOW()
-#endif
-
 /*
  * Globals
  */
@@ -196,9 +178,9 @@ static void exit_odbc_struct(struct object *o)
     if (PIKE_ODBC->flags & PIKE_ODBC_CONNECTED) {
       RETCODE code;
       PIKE_ODBC->flags &= ~PIKE_ODBC_CONNECTED;
-      THREADS_ALLOW();
+      ODBC_ALLOW();
       code = SQLDisconnect(hdbc);
-      THREADS_DISALLOW();
+      ODBC_DISALLOW();
       odbc_check_error("odbc_error", "Disconnecting HDBC", code,
 		       (void (*)(void *))exit_odbc_struct, NULL);
       /* NOTE: Potential recursion above! */
@@ -285,23 +267,23 @@ static void f_create(INT32 args)
   }
   if (PIKE_ODBC->flags & PIKE_ODBC_CONNECTED) {
     PIKE_ODBC->flags &= ~PIKE_ODBC_CONNECTED;
-    THREADS_ALLOW();
+    ODBC_ALLOW();
     code = SQLDisconnect(hdbc);
-    THREADS_DISALLOW();
+    ODBC_DISALLOW();
     /* Disconnect old hdbc */
     odbc_check_error("odbc->create", "Disconnecting HDBC", code, NULL, NULL);
   }
 
   /* FIXME: Support wide strings. */
 
-  THREADS_ALLOW();
+  ODBC_ALLOW();
   code = SQLConnect(hdbc, (unsigned char *)database->str,
 		    DO_NOT_WARN((SQLSMALLINT)database->len),
 		    (unsigned char *)user->str,
 		    DO_NOT_WARN((SQLSMALLINT)user->len),
 		    (unsigned char *)pwd->str,
 		    DO_NOT_WARN((SQLSMALLINT)pwd->len));
-  THREADS_DISALLOW();
+  ODBC_DISALLOW();
   odbc_check_error("odbc->create", "Connect failed", code, NULL, NULL);
   PIKE_ODBC->flags |= PIKE_ODBC_CONNECTED;
   pop_n_elems(args);
@@ -370,9 +352,9 @@ static void f_big_query(INT32 args)
     {
       HDBC hdbc = PIKE_ODBC->hdbc;
       RETCODE code;
-      THREADS_ALLOW();
+      ODBC_ALLOW();
       code = SQLTransact(odbc_henv, hdbc, SQL_COMMIT);
-      THREADS_DISALLOW();
+      ODBC_DISALLOW();
       odbc_check_error("odbc->big_query", "Couldn't commit query",
 		       code, NULL, NULL);
     }
