5b3192e7fc81b859e31398f9da52fa57c837776c
Henrik Grubbström (Grubba) <grubba@grubba.org>
2008-03-14 08:49:51 +0000

    Missed setting the run_time_type field in the dummy identifier.

--- src/interpret.c
+++ src/interpret.c
@@ -2339,6 +2339,7 @@ PMOD_EXPORT int apply_low_safe_and_stupid(struct object *o, INT32 offset)
     };
     copy_shared_string(dummy.name, empty_pike_string);
     copy_pike_type(dummy.type, function_type_string);
+    dummy.run_time_type = PIKE_T_FUNCTION;
     dummy.identifier_flags = IDENTIFIER_PIKE_FUNCTION|IDENTIFIER_HAS_BODY;
     dummy.func.offset = offset;
     dummy.opt_flags = 0;
