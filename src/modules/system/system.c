e68c1724ea468322fc73603063599f022652d7f9
Dan Nelson <dnelson@emsphone.com>
2008-04-21 09:06:54 +0000

    Applied patch enabling resolvepath() on more OSes. Thanks to Dan Nelson <dnelson@emsphone.com>.

--- src/modules/system/system.c
+++ src/modules/system/system.c
@@ -3038,12 +3038,12 @@ PIKE_MODULE_INIT
   ADD_EFUN("readlink", f_readlink,tFunc(tStr,tStr), OPT_EXTERNAL_DEPEND);
   ADD_FUNCTION2("readlink", f_readlink,tFunc(tStr,tStr), 0, OPT_EXTERNAL_DEPEND);
 #endif /* HAVE_READLINK */
-#ifdef HAVE_RESOLVEPATH
+#if defined(HAVE_RESOLVEPATH) || defined(HAVE_REALPATH)
   
 /* function(string:string) */
   ADD_EFUN("resolvepath", f_resolvepath,tFunc(tStr,tStr), OPT_EXTERNAL_DEPEND);
   ADD_FUNCTION2("resolvepath", f_resolvepath,tFunc(tStr,tStr), 0, OPT_EXTERNAL_DEPEND);
-#endif /* HAVE_RESOLVEPATH */
+#endif /* HAVE_RESOLVEPATH || HAVE_REALPATH */
 
   /* function(int|void:int) */
   ADD_EFUN("umask", f_umask, tFunc(tOr(tInt,tVoid),tInt), OPT_SIDE_EFFECT);
