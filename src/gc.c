294b3ff45deb9338bbd8dcec7aab189dd7baf0fe
Henrik Grubbström (Grubba) <grubba@grubba.org>
2008-05-03 16:02:06 +0000

    Proper fix of Autodoc mk II for count_memory().

--- src/gc.c
+++ src/gc.c
@@ -4073,9 +4073,9 @@ void dump_gc_info(void)
  *!   zero.
  *!
  *! @note
- *! The result of @code{Pike.count_memory(0,a,b)} might be larger
- *! than the sum of @code{Pike.count_memory(0,a)} and
- *! @code{Pike.count_memory(0,b)} since @code{a} and @code{b}
+ *! The result of @expr{Pike.count_memory(0,a,b)@} might be larger
+ *! than the sum of @expr{Pike.count_memory(0,a)@} and
+ *! @expr{Pike.count_memory(0,b)@} since @expt{a@} and @expr{b@}
  *! together might reference things that aren't referenced from
  *! anywhere else.
  *!
