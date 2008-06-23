c7acba4449acd50afa711c1798dd20b0b8b94109
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2008-06-23 18:56:34 +0000

    Fixed allocation bug in init_string_builder_copy. This could cause memory corruption in String.Buffer when using `+ on overallocated buffers.
    Also ensure proper wide NUL termination in wide string builders.

--- src/stralloc.c
+++ src/stralloc.c
@@ -1987,7 +1987,7 @@ PMOD_EXPORT void init_string_builder(struct string_builder *s, int mag)
   s->malloced=256;
   s->s=begin_wide_shared_string(256,mag);
   s->s->len=0;
-  s->s->str[0] = 0;
+  low_set_index (s->s, 0, 0);
   s->known_shift=0;
 }
 
@@ -1996,7 +1996,7 @@ PMOD_EXPORT void init_string_builder_alloc(struct string_builder *s, ptrdiff_t l
   s->malloced=length;
   s->s=begin_wide_shared_string(length,mag);
   s->s->len=0;
-  s->s->str[0] = 0;
+  low_set_index (s->s, 0, 0);
   s->known_shift=0;
 }
 
@@ -2004,8 +2004,9 @@ PMOD_EXPORT void init_string_builder_copy(struct string_builder *to,
 					  struct string_builder *from)
 {
   to->malloced = from->malloced;
-  to->s = begin_wide_shared_string (from->s->len, from->s->size_shift);
-  MEMCPY (to->s->str, from->s->str, from->s->len << from->s->size_shift);
+  to->s = begin_wide_shared_string (from->malloced, from->s->size_shift);
+  to->s->len = from->s->len;
+  MEMCPY (to->s->str, from->s->str, (from->s->len + 1) << from->s->size_shift);
   to->known_shift = from->known_shift;
 }
 
@@ -2209,7 +2210,7 @@ PMOD_EXPORT void reset_string_builder(struct string_builder *s)
   s->known_shift=0;
   s->s->len=0;
   /* Ensure NUL-termination */
-  s->s->str[0] = 0;
+  low_set_index (s->s, 0, 0);
 }
 
 PMOD_EXPORT void free_string_builder(struct string_builder *s)
