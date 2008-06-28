d771369d08d5c14278f858710f07f2a9e8462403
Henrik Grubbström (Grubba) <grubba@grubba.org>
2008-06-28 07:00:47 +0000

    Now has a chance at supporting machine code on systems without execinfo.h.

--- src/program.c
+++ src/program.c
@@ -10706,7 +10706,9 @@ PMOD_EXPORT void change_compiler_compatibility(int major, int minor)
 #include <sys/mman.h>
 #endif
 
+#ifdef HAVE_EXECINFO_H
 #include <execinfo.h>
+#endif
 
 void make_area_executable (char *start, size_t len)
 {
