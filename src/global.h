71690fdac31c50405773ad4780a6e0e5503952dc
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2008-06-29 11:45:54 +0000

    Chars are signed, but only the very wide ones.

--- src/global.h
+++ src/global.h
@@ -402,9 +402,11 @@ struct b16_t_s { B8_T x,y; };
 #error Unsupported type chosen for pike floats.
 #endif
 
+/* Conceptually a char is a 32 bit signed value. Implementationwise
+ * that means that the shorter ones don't have space for the sign bit. */
 typedef unsigned char p_wchar0;
 typedef unsigned INT16 p_wchar1;
-typedef unsigned INT32 p_wchar2;
+typedef signed INT32 p_wchar2;
 
 typedef struct p_wchar_p
 {
