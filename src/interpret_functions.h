4476dd6bfa0d5813d675ed9006a22e3a517f0e6a
Martin Stjernholm (Mast) <mast@pike.ida.liu.se>
2008-06-30 13:46:05 +0000

    Use PRINTPTRDIFFT.

--- src/interpret_functions.h
+++ src/interpret_functions.h
@@ -1086,8 +1086,9 @@ OPCODE0_TAIL(F_CLEANUP_SYNCH_MARK, "cleanup synch mark", I_UPDATE_SP|I_UPDATE_M_
 	if (Pike_sp - *Pike_mark_sp > 0) /* not always same as Pike_sp > *Pike_mark_sp */
 	/* Some attempt to recover, just to be able to report the backtrace. */
 	  pop_n_elems(Pike_sp - *Pike_mark_sp);
-	Pike_fatal("Stack out of synch - should be %ld, is %ld.\n",
-	      DO_NOT_WARN((long)should), DO_NOT_WARN((long)is));
+	Pike_fatal("Stack out of synch - "
+		   "should be %"PRINTPTRDIFFT"d, is %"PRINTPTRDIFFT"d.\n",
+		   should, is);
       }
     }
   });
