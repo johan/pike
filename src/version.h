1d51b127ca2cfcc12c2657fca6d37ac4802ef512
Peter Bortas <peter@bortas.org>
2008-10-20 12:46:34 +0000

    release number bumped to 51 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 50
+#define PIKE_BUILD_VERSION 51
 
 /* Prototypes begin here */
 void f_version(INT32 args);
