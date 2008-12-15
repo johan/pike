9c5aac2ee7073c733a8718c448e7fa079ab4129a
Build system <distmaker>
2008-12-15 09:57:48 +0000

    release number bumped to 105 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 104
+#define PIKE_BUILD_VERSION 105
 
 /* Prototypes begin here */
 void f_version(INT32 args);
