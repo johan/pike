25aae13065b8694e7643a843da3c8c86985e0937
Henrik Grubbström (Grubba) <grubba@grubba.org>
2009-02-06 18:53:43 +0000

    POSIX says that recv(3N) should fail with EINVAL if there's no OOB data available.

--- src/modules/files/file.c
+++ src/modules/files/file.c
@@ -632,8 +632,9 @@ static struct pike_string *do_read_oob(int fd,
 	bytes_read+=i;
 	if(!all) break;
       }
-      else if(i==0)
+      else if ((i==0) || (e == EINVAL))
       {
+	/* No out of band data available. */
 	break;
       }
       else if(e != EINTR)
