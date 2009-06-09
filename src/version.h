46594159c0f30bd7926a5b1715422103a7dfc165
Peter Bortas <peter@bortas.org>
2009-06-09 14:24:22 +0000

    release number bumped to 293 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 292
+#define PIKE_BUILD_VERSION 293
 
 /* Prototypes begin here */
 void f_version(INT32 args);
