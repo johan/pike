a5e0268f756f92404a69a274cf4301c21c91b865
Martin Stjernholm <mast@lysator.liu.se>
2009-11-15 00:58:35 +0000

    Fixed reinitialization of the charset after reconnect.

--- src/modules/Mysql/precompiled_mysql.h
+++ src/modules/Mysql/precompiled_mysql.h
@@ -59,13 +59,7 @@ struct precompiled_mysql {
   MYSQL		*mysql;
   struct pike_string	*host, *database, *user, *password;	/* Reconnect */
   struct mapping   *options;
-#ifndef HAVE_MYSQL_SET_CHARACTER_SET
-  /* Old libs (< 4.1.13) doesn't support changing the connection
-   * charset. We emulate it by storing the charset ourselves. Note
-   * that this doesn't work with mysql_real_escape_string, but that
-   * function isn't used. */
   struct pike_string *conn_charset;
-#endif
 };
 
 struct precompiled_mysql_result {
