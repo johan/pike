1fea8100cfdd72e8b627f31e2ea4569b078c5cb8
Martin Stjernholm <mast@lysator.liu.se>
2009-11-17 01:25:04 +0000

    Fixed bug that could cause posix highres cpu time to remain undetected.

--- src/pike_rusage.h
+++ src/pike_rusage.h
@@ -10,6 +10,9 @@
 
 #include "global.h"
 
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
 #ifdef HAVE_TIME_H
 #include <time.h>
 #endif
@@ -72,14 +75,14 @@
 #    define posix_thread_gct get_cpu_time
 #    define posix_thread_gct_res get_cpu_time_res
 #    define GCT_IS_POSIX_THREAD
+#  elif defined (MIGHT_HAVE_POSIX_THREAD_GCT)
+#    define GCT_RUNTIME_CHOICE
 #  elif FB_CPU_TIME_IS_THREAD_LOCAL == PIKE_YES
 #    define cpu_time_is_thread_local 1
 #    define fallback_gct_impl get_cpu_time_impl
 #    define fallback_gct get_cpu_time
 #    define fallback_gct_res get_cpu_time_res
 #    define GCT_IS_FALLBACK
-#  elif defined (MIGHT_HAVE_POSIX_THREAD_GCT)
-#    define GCT_RUNTIME_CHOICE
 #  elif defined (HAVE_POSIX_PROCESS_GCT)
 #    define cpu_time_is_thread_local 0
 #    define posix_process_gct_impl get_cpu_time_impl
