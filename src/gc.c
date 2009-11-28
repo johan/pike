a707a8872646e6cea810292b27a2c03caad7515d
Martin Stjernholm <mast@lysator.liu.se>
2009-11-28 13:53:59 +0000

    Adhere a bit better to defacto name conventions.

--- src/gc.c
+++ src/gc.c
@@ -2470,7 +2470,7 @@ void gc_delayed_free(void *a, int type)
   m->flags |= GC_GOT_DEAD_REF;
 }
 
-int gc_mark_func(void *a DO_IF_DEBUG (COMMA int type))
+int real_gc_mark(void *a DO_IF_DEBUG (COMMA int type))
 {
   struct marker *m;
 
