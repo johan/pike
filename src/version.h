fcb65e0a9ca02014b3587802c24e14d726356f23
Peter Bortas <peter@bortas.org>
2010-01-27 14:53:05 +0000

    release number bumped to 403 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 402
+#define PIKE_BUILD_VERSION 403
 
 /* Prototypes begin here */
 void f_version(INT32 args);
