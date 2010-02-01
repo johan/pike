661cfdda48e4c9663d2093cbeb0dfeae3fb5e5cc
Build system <distmaker>
2010-02-01 16:12:56 +0000

    release number bumped to 404 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 403
+#define PIKE_BUILD_VERSION 404
 
 /* Prototypes begin here */
 void f_version(INT32 args);
