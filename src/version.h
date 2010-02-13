68d2d0090c8d23a1f07a6f3a873faaef48d953a6
Peter Bortas <peter@bortas.org>
2010-02-13 20:13:51 +0000

    release number bumped to 411 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 410
+#define PIKE_BUILD_VERSION 411
 
 /* Prototypes begin here */
 void f_version(INT32 args);
